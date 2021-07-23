67062c02affd4b584684bde458623826c2fbb2a9
andyxiong11 <xiongyexing1999@163.com>
2021-07-23 15:25:01 +0800

    更新

--- python/3_/3_1.py
+++
@@ -1,9 +0,0 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
-print(bicycles[0])
-print(bicycles[0].title())
-# -1表示最后一个元素
-print(bicycles[-1].title())
-print(bicycles[-2].title())
-message = "My first bicycle was a " + bicycles[0].title() + "."
-print(message)
