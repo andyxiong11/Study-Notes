aaf1d0c5f2536291e92a118be55bd11e126b2ef9
andyxiong11 <xiongyexing1999@163.com>
2022-09-28 18:04:41 +0800

    新增leetcode回文数代码及笔记

---
+++ LeetCode/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(Solution.isPalindrome(13421));;
+    }
+    public static boolean isPalindrome(int x) {
+        boolean flag = true;
+        String str =String.valueOf(x);
+        int l = str.length();
+        if(x>0){
+            for(int i = 0;i < (l/2); i++){
+                if(flag == false){
+                    break;
+                }
+                while(i != (l/2)){
+                    if(str.charAt(i) == str.charAt(l-i-1)){
+                        break;
+                    }
+                    else
+                        flag = false;
+                        break;
+                }
+            }
+        }
+        else{
+            flag =false;
+        }
+        return flag;
+    }
+}
