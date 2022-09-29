8ef96c3bea627da59db51dc253688de164fc5829
andyxiong11 <xiongyexing1999@163.com>
2022-09-29 10:01:32 +0800

    根据官解优化，输入100有问题

--- LeetCode/Solution.java
+++ LeetCode/Solution.java
@@ -1,29 +1,51 @@
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Solution.isPalindrome(13421));;
+        System.out.println(Solution.isPalindrome(100));;
     }
     public static boolean isPalindrome(int x) {
-        boolean flag = true;
+        //第二种
+        boolean flag = false;
         String str =String.valueOf(x);
         int l = str.length();
-        if(x>0){
-            for(int i = 0;i < (l/2); i++){
-                if(flag == false){
-                    break;
-                }
-                while(i != (l/2)){
-                    if(str.charAt(i) == str.charAt(l-i-1)){
-                        break;
-                    }
-                    else
-                        flag = false;
-                        break;
-                }
+        int y = 0;
+        int x1 = x;
+        while(x1>y){
+            int g = x1 % 10;
+            x1 /= 10;
+            y = y*10 + g;
+        }
+        if(l%2 == 0){
+            if(x1 == y){
+                flag = true;
             }
         }
-        else if(x<0){
-            flag =false;
+        else{
+            if(x1 == (y/10)){
+                flag = true;
+            }
         }
+        //第一种解法
+        // boolean flag = false;
+        // String str =String.valueOf(x);
+        // int l = str.length();
+        // if(x>0){
+        //     for(int i = 0;i < (l/2); i++){
+        //         if(flag == false){
+        //             break;
+        //         }
+        //         while(i != (l/2)){
+        //             if(str.charAt(i) == str.charAt(l-i-1)){
+        //                 break;
+        //             }
+        //             else
+        //                 flag = false;
+        //                 break;
+        //         }
+        //     }
+        // }
+        // else if(x<0){
+        //     flag =false;
+        // }
         return flag;
     }
 }
