54cb43f73737f33c89907360ec7b9443fe22448d
andyxiong11 <xiongyexing1999@163.com>
2022-09-29 17:39:32 +0800

    罗马数字转整数，增加官解：HashMap代码

---
+++ LeetCode/13.罗马数字转整数/Solution_leetcode.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class Solution_leetcode {
+    public static void main(String[] args) {
+        String s1 = new String("MCMXCIV");
+        System.out.println(Solution_leetcode.romanToInt(s1));;
+    }
+    public static int romanToInt(String s) {
+        int l = s.length();
+        int num = 0;
+        int x = 0;
+        int y = 0;
+        HashMap<Character, Integer> Sites = new HashMap<Character, Integer>();
+        Sites.put('I', 1);
+        Sites.put('V', 5);
+        Sites.put('X', 10);
+        Sites.put('L', 50);
+        Sites.put('C', 100);
+        Sites.put('D', 500);
+        Sites.put('M', 1000);
+        for (int i = 0; i < l; i++) {
+            if((i>=1) && (Sites.get(s.charAt(i))>Sites.get(s.charAt(i-1)))){
+                num = num - Sites.get(s.charAt(i-1)) - Sites.get(s.charAt(i-1)) + Sites.get(s.charAt(i));
+            }else{
+                num += Sites.get(s.charAt(i));
+            }
+        }
+        return num;
+    }
+}
