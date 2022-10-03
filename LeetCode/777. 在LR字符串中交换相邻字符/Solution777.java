fc0fa4bfa45de121b9e9ebc2feb5d85d9014dd75
andyxiong11 <xiongyexing1999@163.com>
2022-10-03 11:14:56 +0800

    输入"RL""LR"，输出结果不对

--- LeetCode/777. 在LR字符串中交换相邻字符/Solution777.java
+++ LeetCode/777. 在LR字符串中交换相邻字符/Solution777.java
@@ -1,22 +1,26 @@
 public class Solution777 {
     public static void main(String[] args) {
+        //System.out.println(Solution777.canTransform("XLLR", "LXLX"));
         System.out.println(Solution777.canTransform("RXXLRXRXL", "XRLXXRRLX"));
     }
+
     public static boolean canTransform(String start, String end) {
         boolean flag = true;
-        if(start.length()==end.length()){
-            if(start.length()<2 && (start.charAt(0) != end.charAt(0))){
+        if (start.length() == end.length()) {
+            if (start.length() < 2 && (start.charAt(0) != end.charAt(0))) {
                 return flag = false;
-            }else{
+            } else {
                 for (int l = 0; l < start.length(); l++) {
-                    if ((start.charAt(l) != end.charAt(l + 1))) {
-                        if (start.charAt(l) != end.charAt(l)) {
+                    if ((start.charAt(l) != end.charAt(l))&& l+1<start.length()) {
+                        if (start.charAt(l) != end.charAt(l + 1)) {
                             return flag = false;
-                        }else if (start.charAt(l + 1) == end.charAt(l + 1)) {
+                        } else if ((start.charAt(l + 1) == end.charAt(l))) {
                             l++;
+                        } else {
+                            return flag = false;
                         }
-                    }else{
-                        l++;
+                    }else if(l+1>=start.length()){
+                        return flag=false;
                     }
                 }
             }
