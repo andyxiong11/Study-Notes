2d2f54725327f3c3067f8449b7ca5b0226d5214f
andyxiong11 <xiongyexing1999@163.com>
2022-10-06 19:12:21 +0800

    输入"XXXXXLXXXX", "LXXXXXXXXX"，输出结果不对，题目理解错误

--- LeetCode/777. 在LR字符串中交换相邻字符/Solution777.java
+++ LeetCode/777. 在LR字符串中交换相邻字符/Solution777.java
@@ -1,26 +1,28 @@
 public class Solution777 {
     public static void main(String[] args) {
-        //System.out.println(Solution777.canTransform("XLLR", "LXLX"));
-        System.out.println(Solution777.canTransform("RXXLRXRXL", "XRLXXRRLX"));
+        // System.out.println(Solution777.canTransform("XLLR", "LXLX"));
+        System.out.println(Solution777.canTransform("XXXXXLXXXX", "LXXXXXXXXX"));
     }
 
     public static boolean canTransform(String start, String end) {
         boolean flag = true;
-        if (start.length() == end.length()) {
+        if ((start.length() == end.length())) {
             if (start.length() < 2 && (start.charAt(0) != end.charAt(0))) {
                 return flag = false;
             } else {
                 for (int l = 0; l < start.length(); l++) {
-                    if ((start.charAt(l) != end.charAt(l))&& l+1<start.length()) {
-                        if (start.charAt(l) != end.charAt(l + 1)) {
-                            return flag = false;
-                        } else if ((start.charAt(l + 1) == end.charAt(l))) {
-                            l++;
+                    if ((start.charAt(l) != end.charAt(l)) && l + 1 < start.length()) {
+                        if (start.charAt(l) == end.charAt(l + 1) && start.charAt(l + 1) == end.charAt(l)) {
+                            if (start.charAt(l) == 'X' && start.charAt(l + 1) == 'L') {
+                                l++;
+                            } else if (start.charAt(l) == 'R' && start.charAt(l + 1) == 'X') {
+                                l++;
+                            }   
                         } else {
                             return flag = false;
                         }
-                    }else if(l+1>=start.length()){
-                        return flag=false;
+                    } else if ((start.charAt(l) != end.charAt(l)) && l + 1 >= start.length()) {
+                        return flag = false;
                     }
                 }
             }
