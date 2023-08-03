cf22374315e75518c176fde4d6adfa2a35343e64
andyxiong11 <xiongyexing1999@163.com>
2023-08-03 16:09:15 +0800

    更新脚手架文件夹

---
+++ 07Vue/19_脚手架/src/main.js
@@ -0,0 +1,14 @@
+/*
+该文件是整个项目的入口文件
+件*/
+//引入Vue
+import Vue from 'vue'
+//引入App组件，它是所有组件的父组件
+import App from './App.vue'
+//关闭vue的生产提示
+Vue.config.productionTip = false
+//创建Vue实例对象--vm
+new Vue({
+  //下面这行代码一会解释，完成了这个功能：将App组件放入容器中
+  render: h => h(App),
+}).$mount('#app')
