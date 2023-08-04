3c082613e3b752ddf0856a6816e2617819fdebc6
andyxiong11 <xiongyexing1999@163.com>
2023-08-04 22:44:07 +0800

    0804 p65 6:10暂存

---
+++ 07Vue/19_脚手架/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <School/>
+    <School/>
+    <School/>
+  </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+export default {
+  name: 'App',
+  components:{School}
+}
+</script>
