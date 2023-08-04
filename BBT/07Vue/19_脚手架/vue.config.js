3ece2804723c99d64e4b2ac88dd11d95eecab487
andyxiong11 <xiongyexing1999@163.com>
2023-08-04 21:17:56 +0800

    0803 p64加0804部分内容结束

--- BBT/07Vue/19_脚手架/vue.config.js
+++ BBT/07Vue/19_脚手架/vue.config.js
@@ -1,4 +1,17 @@
+//下面为脚手架默认生成
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+//以下为自定义
+module.exports = { //common.js的暴露
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/index/main.js',//不能注释掉默认配置
+      //可以改为 entry: 'src/peiqi.js',
+    }
+  },
+  lintOnSave : false//关闭语法检查
+}
