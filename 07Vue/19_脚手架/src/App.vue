4ca171571df04cf1aee7d6a1c0dc72576cfe9357
andyxiong11 <xiongyexing1999@163.com>
2023-08-05 22:52:09 +0800

    0805 p69结束暂存，p72

---
+++ 07Vue/19_脚手架/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Student/>
+    <hr>
+    <School/>
+  </div>
+</template>
+
+<script>
+  import School from './components/School.vue'
+  import Student from "./components/Student.vue";
+  export default {
+    name: 'App',
+    components:{
+      Student,
+      School
+    }
+  }
+</script>
