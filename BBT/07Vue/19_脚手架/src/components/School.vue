38de30aea245fb2269352749d52e3b5549afb6e2
andyxiong11 <xiongyexing1999@163.com>
2023-08-05 22:52:09 +0800

    0805 p69结束暂存，p72

---
+++ BBT/07Vue/19_脚手架/src/components/School.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "School",
+    data(){
+      return {
+        name: "光谷",
+        address: '武汉',
+      }
+    },
+  }
+</script>
+
+<style>
+  /* 所有组件写的样式最后要汇总到一个文件
+     如果School组件的类名与此处的类名一致，则背景色均为橙色，因为在App.vue中后引入Student.vue */
+  .demo{
+    background-color: skyblue;
+  }
+</style>
