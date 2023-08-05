3c9de70768568184b224c238950db5cbbe593974
andyxiong11 <xiongyexing1999@163.com>
2023-08-05 21:27:39 +0800

    mixin学完

---
+++ BBT/07Vue/19_脚手架/04_src_mixin混入/components/School.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 @click="showName">学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+  //引入mixin
+  import { mixin,mixin2 } from "../mixin.js";
+  export default {
+    name: "School",
+    data(){
+      console.log(this);//组件的实例对象
+      return {
+        name: "光谷",
+        address: '武汉',
+        x: 200//数据和函数与mixin中有冲突时，以组件中的配置为准，钩子函数则是都执行
+      }
+    },
+    //因为Student和School组件有公共的函数，放到mixin.js中
+    // methods: {
+    //   showName(){
+    //     alert(this.name);
+    //   }
+    // },
+    mixins: [mixin,mixin2]
+  }
+</script>
