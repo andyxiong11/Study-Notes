038c278194211b5a56befbbfee2572af72a57c6a
andyxiong11 <xiongyexing1999@163.com>
2023-08-06 15:05:14 +0800

    将写好的html页面中的body结构和样式放入App.vue，并拆分到对应的组件

---
+++ 07Vue/19_脚手架/src/components/MyList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="todo-main">
+    <!-- 第三步将MyList.vue中的li标签替换为<MyItem/> -->
+    <!-- <li>
+      <label>
+        <input type="checkbox" />
+        <span>xxxxx</span>
+      </label>
+      <button class="btn btn-danger" style="display: none">删除</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox" />
+        <span>yyyy</span>
+      </label>
+      <button class="btn btn-danger" style="display: none">删除</button>
+    </li> -->
+    <MyItem/>
+    <MyItem/>
+</ul>
+</template>
+
+<script>
+  import MyItem from './MyItem.vue';
+  export default {
+    name: "MyList",
+    components:{
+      MyItem
+    }
+  }
+</script>
+
+<style scoped>
+  .todo-main {
+    margin-left:0px;
+    border:1px solid #ddd;
+    border-radius:2px;
+    padding:0px;
+  }
+  .todo-empty {
+    height:40px;
+    line-height:40px;
+    border:1px solid #ddd;
+    border-radius:2px;
+    padding-left:5px;
+    margin-top:10px;
+  }
+
+</style>
