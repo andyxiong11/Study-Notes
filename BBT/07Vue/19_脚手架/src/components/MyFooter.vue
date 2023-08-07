cb4e0a1ec465a18c364c40e7f67460d8ff4ad618
andyxiong11 <xiongyexing1999@163.com>
2023-08-07 22:57:47 +0800

    去除注释

--- BBT/07Vue/19_脚手架/src/components/MyFooter.vue
+++ BBT/07Vue/19_脚手架/src/components/MyFooter.vue
@@ -1,20 +1,9 @@
 <template>
-  <!-- <div class="todo-footer" v-show="total"> -->
-  <!-- 没有选项时不展示 -->
   <div class="todo-footer" v-show="total">
     <label>
-      <!-- <input type="checkbox" /> -->
-      <!-- 选项全部勾选时，勾选全部 -->
-      <!-- <input type="checkbox" :checked="doneTotal === total" /> -->
-      <!-- 结构中不建议用表达式 -->
-      <!-- <input type="checkbox" :checked="isAll"/> -->
-      <!-- 勾选全部时，选项全部勾选 -->
       <input type="checkbox" :checked="isAll" @change="checkAll"/>
-      <!-- 采用下面的方法，勾选全选时会报错，因为下面的计算属性没有setter不允许修改，需要安装64行修改 -->
-      <!-- <input type="checkbox" v-model="isAll"/> -->
     </label>
     <span> 
-      <!-- <span>己完成0</span> / 全部2  -->
       <span>己完成{{ doneTotal }}</span> / 全部{{ total }}
     </span>
     <button class="btn btn-danger" @click="cleanAll">清除己完成任务</button>
@@ -32,52 +21,19 @@
 
       //统计勾选的数量
       doneTotal(){
-        //第一种方法
-        // let i = 0;
-        // this.todos.forEach((todo) => {
-        //   if(todo.done) i++;
-        // })
-        // return i
-
-        //第二种方法
-        //return this.todos.filter((todo) => todo.done).length
-
-        //第三种方法
         return this.todos.reduce((pre,current) => {
-          //todos有3个数据，运行3次
-          //第一次运行的pre为第二个参数0，后面每一次的pre取第一次运行的返回值，因为没有return，所以undefined   
-          //console.log('@',pre);//0 undefined undefined 
-          
-          //current 每一个todo对象,勾选时+1
           return pre + (current.done ? 1 : 0)
         },0)
-        //精简
-        //return this.todos.reduce((pre,current) => pre + (current.done ? 1 : 0),0)
       },
 
       //选项全部勾选时，勾选全部
       isAll(){
-        // return this.doneTotal === this.total;
-        //修复bug，当没有选项时不能全选
         return this.doneTotal === this.total && this.total > 0;
       },
-      //以下为14行写法必须写的，且不用写checkAll方法
-      // isAll:{
-      //   get(){
-      //     return this.doneTotal === this.total && this.total > 0;
-      //   },
-      //   set(value){
-      //     //console.log(value);
-      //     this.checkAllTodo(value);
-      //   }
-      // }
     },
     methods: {
       //勾选全部时，选项全部勾选
       checkAll(event){
-        //是否勾选
-        //console.log(event.target.checked);
-
         this.checkAllTodo(event.target.checked);
       },
       cleanAll(){
--- BBT/07Vue/19_脚手架/src/components/MyItem.vue
+++ BBT/07Vue/19_脚手架/src/components/MyItem.vue
@@ -1,19 +1,10 @@
 <template>
   <li>
     <label>
-      <!-- 选项默认勾选 :checked="todo.done"  -->
-      <!-- <input type="checkbox" :checked="todo.done" @click="handleCheck(todo.id)"/> -->
-      <!-- 检测单选框的改变用@change比较好 -->
       <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
-      <!-- 也可以通过v-model修改todo.done，不用调用App组件的函数，但是不建议直接修改props -->
-      <!-- <input type="checkbox" :checked="todo.done" v-model="todo.done"/> -->
-      
-      <!-- <span>xxxxx</span> -->
       <span>{{ todo.title }}</span>
     </label>
 
-    <!-- <button class="btn btn-danger" style="display: none">删除</button> -->
-    <!-- 在css中添加样式鼠标悬停显示按钮 delete是js关键词 ，不能用在@click-->
     <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
   </li>
 </template>
@@ -21,25 +12,14 @@
 <script>
   export default {
     name: "MyItem",
-    //声明接收todo对象
     props: ['todo','checkTodo','deleteTodo'],
-    // mounted() {
-    //   //console.log(this.todo);
-    // },
     methods: {
-      //以下检测勾选的思路：在App组件写一个取反的函数，通过MyList传递给MyItem调用
       handleCheck(id){
-        //console.log(id);
-
-        //通知App组件将对应的todo对象的done值取反
         this.checkTodo(id);
       },
       //删除
       handleDelete(id){
         if(confirm('确定删除吗？')){
-          //console.log(id);
-
-          //通知App组件将对应的todo删除
           this.deleteTodo(id);
         }
       }
