08d43f1fb0001f9fadc2f2d3442c709f2db7689d
andyxiong11 <xiongyexing1999@163.com>
2023-08-07 22:36:55 +0800

    自定义事件总结

--- 07Vue/19_脚手架/src/App.vue
+++ 07Vue/19_脚手架/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }},学生的姓名{{ studentName }}</h1>
     <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
     <School :getSchoolName="getSchoolName"/>
     
@@ -8,14 +8,19 @@
     <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第一种写法：@或v-on） -->
     <!--给student组件的实例对象vc绑定atguigu事件-->
     <!--<Student v-on:atguigu.once="getStudentName"/> .once 一次性-->
-    <!-- <Student @atguigu.once="getStudentName"/> -->
+    <Student @atguigu="getStudentName"/><!--@atguigu.once 一次性-->
 
     <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第二种写法：ref） -->
     <!-- <Student ref="student"/> -->
     <!-- 第二种方法的好处是可以加上定时器 -->
 
+
     <!-- 以下代码仅用于测试解绑多个事件 -->
-    <Student @atguigu="getStudentName" @demo="m1"/>
+    <!-- <Student @atguigu="getStudentName" @demo="m1"/> -->
+
+    
+    <!-- 给组件添加原生事件 -->
+    <!-- <Student @click.native="show"/> -->
   </div>
 </template>
 
@@ -30,7 +35,8 @@
     },
     data(){
       return {
-        msg: '你好啊'
+        msg: '你好啊',
+        studentName:  ''
       }
     },
     methods: {
@@ -39,10 +45,17 @@
       },
       getStudentName(name,...params){
         console.log("App收到了学生名",name,params);
+        //将自定义事件的name展示到页面上，第一种写法
+        this.studentName = name
       },
-      m1(){
-        console.log("demo事件被触发了");
-      }
+
+      // m1(){
+      //   console.log("demo事件被触发了");
+      // }
+
+      // show(){
+      //   alert(调用了原生事件)
+      // }
     },
     mounted() {//App组件挂载完时调用
       // this.$refs.student.$on('atguigu',this.getStudentName)//绑定自定义事件
@@ -53,6 +66,13 @@
       // setTimeout(() => {
       //   this.$refs.student.$on('atguigu',this.getStudentName)
       // }, 3000);
+
+      //将自定义事件的name展示到页面上，第二种写法，但是要写成箭头函数，否则this指向有问题
+      this.$refs.student.$on('atguigu',function(name,...params){
+        console.log("App收到了学生名",name,params);
+        // console.log(this);//student组件的vc谁触发atguigu事件，回调函数的this指向谁
+        this.studentName = name;//空
+      })
     },
   }
 </script>
--- 07Vue/19_脚手架/src/components/Student.vue
+++ 07Vue/19_脚手架/src/components/Student.vue
@@ -3,7 +3,7 @@
     <h2>学生姓名：{{name}}</h2>
     <h2>学生性别：{{sex}}</h2>
     <button @click="sendStudentName">把学生名给App</button>
-    <button @click="unbind">解绑Student所有事件</button>
+    <button @click="unbind">解绑atguigu事件</button>
 
     <!-- 以下代码仅用于验证销毁当前Student组件的实例对象VC，销毁后所有Student实例的自定义事件全都不奏效，2023年脚手架优化原生事件也不奏效 -->
     <h2>当前数值为：{{ number }}</h2>
@@ -28,13 +28,16 @@
         this.$emit('atguigu',this.name,666,888,999);
 
         //以下代码仅用于测试解绑多个事件
-        this.$emit('demo');
+        //this.$emit('demo');
       },
       unbind(){
-        //this.$off('atguigu') //解绑一个自定义事件
+        this.$off('atguigu') //解绑一个自定义事件
+
         //this.$off(['atguigu','demo']); //解绑多个自定义事件
-        this.$off(); //解绑所有事件
+        
+        // this.$off(); //解绑所有事件
       },
+
       //以下代码仅用于验证销毁当前Student组件的实例对象VC，销毁后所有Student实例的自定义事件全都不奏效，2023年脚手架优化原生事件也不奏效
       //main.js中也有销毁验证
       death(){
