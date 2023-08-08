0627f60973ea82df60b9e8a8a95ff728345dcd15
andyxiong11 <xiongyexing1999@163.com>
2023-08-08 22:21:26 +0800

    将10_src_组件自定义事件复制到src文件夹并删除注释

---
+++ 07Vue/19_脚手架/src/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="school">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "School",
+    data(){
+      return {
+        name: "光谷",
+        address: '武汉',
+      }
+    },
+  }
+</script>
+
+<style>
+  .school{
+    background-color: skyblue;
+    padding: 5px;
+  }
+</style>
