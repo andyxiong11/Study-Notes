529d6436c419cead60266f75465255431dc19531
andyxiong11 <xiongyexing1999@163.com>
2023-08-13 17:05:10 +0800

    配置代理服务器学完，并将src文件夹重命名为“19_src_配置代理服务器”

---
+++ 07Vue/19_脚手架/19_src_配置代理服务器/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="root">
+    <button @click="getStudents">获取学生信息</button>
+    <button @click="getCars">获取汽车信息</button>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: "App",
+    methods:{
+      getStudents(){
+        //axios.get('http://localhost:5000/students').then(
+        //上面请求会报错，需要通过代理服务器
+        // axios.get('http://localhost:8080/students').then(
+        // 下面需要修改代理配置，不然找不到localhost:5000/atguigu/students
+        axios.get('http://localhost:8080/atguigu/students').then(
+          response => {
+            console.log('请求成功了',response.data);
+          },
+          error => {
+            console.log('请求失败了',error.message);
+          }
+        )
+      },
+      getCars(){
+        axios.get('http://localhost:8080/demo/cars').then(
+          response => {
+            console.log('请求成功了',response.data);
+          },
+          error => {
+            console.log('请求失败了',error.message);
+          }
+        )
+      }
+    }
+  };
+</script>
