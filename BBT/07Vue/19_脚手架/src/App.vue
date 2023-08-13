4cdc69738e531db643895bd3fcd78ee435f21d47
andyxiong11 <xiongyexing1999@163.com>
2023-08-13 22:57:11 +0800

    0813 p106暂存，预计p107结束

---
+++ BBT/07Vue/19_脚手架/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+  import Count from "./components/Count.vue";
+  export default {
+    name: "App",
+    components: {Count},
+  };
+</script>
