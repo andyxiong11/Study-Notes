3aa782783410db98cbe334dbd4bd5c4386a9e022
andyxiong11 <xiongyexing1999@163.com>
2023-08-17 22:47:32 +0800

    路由的几个注意点

--- 07Vue/19_脚手架/src/router/index.js
+++ 07Vue/19_脚手架/src/router/index.js
@@ -1,14 +1,14 @@
 //该文件专门用于创建整个应用的路由器
 import VueRouter from 'vue-router'
 //引入组件
-import About from '../components/About.vue'
-import Home from '../components/Home.vue'
+import About from '../pages/About.vue'
+import Home from '../pages/Home.vue'
 
 //创建一个路由器
 // const router = new VueRouter({
 //创建并暴露一个路由器
 export default new VueRouter({
-  routers:[
+  routes:[
     {
       path:'/about',
       component:About
