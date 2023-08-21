cad61e9393d6841527f82742f57a8d08bc104f99
andyxiong11 <xiongyexing1999@163.com>
2023-08-21 22:35:36 +0800

    初始化src文件夹

---
+++ 07Vue/19_脚手架/src/router/index.js
@@ -0,0 +1,47 @@
+//该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+//引入组件
+import About from '../pages/About.vue'
+import Home from '../pages/Home.vue'
+import News from '../pages/News.vue'
+import Message from '../pages/Message.vue'
+import Detail from '../pages/Detail.vue'
+
+//创建一个路由器
+// const router = new VueRouter({
+//创建并暴露一个路由器
+export default new VueRouter({
+  routes:[
+    {
+      name:'about',
+      path:'/about',
+      component:About
+    },
+    {
+      name:'home',
+      path:'/home',
+      component:Home,
+      children:[
+        {
+          name:'news',
+          path:'news',//注意不要斜杠
+          component:News
+        },
+        {
+          name:'message',
+          path:'message',//注意不要斜杠
+          component:Message,
+          children:[
+            {
+              name:'detail',
+              path:'detail/:id/:title',//注意不要斜杠
+              component:Detail
+            },
+          ]
+        }
+      ]
+    },
+  ]
+})
+
+// export default router
