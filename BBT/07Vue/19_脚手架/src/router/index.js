151126d78cdb551198096c06616e126caea996e6
andyxiong11 <xiongyexing1999@163.com>
2023-08-21 22:55:00 +0800

    路由的props参数写完

--- BBT/07Vue/19_脚手架/src/router/index.js
+++ BBT/07Vue/19_脚手架/src/router/index.js
@@ -35,7 +35,18 @@ export default new VueRouter({
             {
               name:'detail',
               path:'detail/:id/:title',//注意不要斜杠
-              component:Detail
+              component:Detail,
+
+              //props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件.
+              // props:{a:'1',b:'hello'},
+
+              //props的第二种写法，值为布尔值，若布尔值为真，就会把该路由组件收到的所有params参数，以props的形式传给Detail组件，
+              // props:true
+
+              //props的第三种写法，值为函数,message组件中要改为query。该函数返回的对象中每一组key-value都会通过props传给Detail组件
+              props($route){
+                return {id:$route.query.id,title:$route.query.title}
+              }
             },
           ]
         }
