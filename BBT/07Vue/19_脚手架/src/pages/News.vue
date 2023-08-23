7951357b9ad3ec41ea3fcf69ff50cd4809f5aea1
andyxiong11 <xiongyexing1999@163.com>
2023-08-23 20:30:33 +0800

    两个新的生命周期钩子写完

--- BBT/07Vue/19_脚手架/src/pages/News.vue
+++ BBT/07Vue/19_脚手架/src/pages/News.vue
@@ -1,5 +1,6 @@
 <template>
   <ul>
+    <li :style = "{opacity}">欢迎学习</li>
     <li>news001 <input type="text"></li>
     <li>news002 <input type="text"></li>
     <li>news003 <input type="text"></li>
@@ -9,8 +10,36 @@
 <script>
   export default {
     name: "News",
-    beforeDestroy() {
-      console.log("News组件即将被销毁");
+    data(){
+      return{
+        opacity: 1,
+      }
+    },
+    // beforeDestroy() {
+    //   console.log("News组件即将被销毁");
+    //    clearInterval(this.timer)
+    // },
+    // mounted() {
+    //   this.timer = setInterval(() => {
+    //     this.opacity -= 0.01;
+    //     if(this.opacity <= 0){
+    //         this.opacity = 1;
+    //     }
+    //   },16)
+    // },
+    activated() {
+      console.log("News组件被激活了");
+      this.timer = setInterval(() => {
+        console.log("@");
+        this.opacity -= 0.01;
+        if(this.opacity <= 0){
+            this.opacity = 1;
+        }
+      },16)
+    },
+    deactivated() {
+      console.log("News组件失活了");
+      clearInterval(this.timer)
     },
   }
 </script>
