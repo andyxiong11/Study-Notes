2eb07d66dcf7904a72508255d917f17d01c947dd
andyxiong11 <xiongyexing1999@163.com>
2023-08-23 08:54:25 +0800

    0822暂存

--- BBT/07Vue/19_脚手架/src/App.vue
+++ BBT/07Vue/19_脚手架/src/App.vue
@@ -6,21 +6,19 @@
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
-          <!-- 原始html中我们使用a标签实现页面的跳转 -->
-          <!-- <a class="list-group-item active" href="./about.html">About</a>
-          <a class="list-group-item" href="./home.html">Home</a> -->
+					<!-- 原始html中我们使用a标签实现页面的跳转 -->
+          <!-- <a class="list-group-item active" href="./about.html">About</a> -->
+          <!-- <a class="list-group-item" href="./home.html">Home</a> -->
 
-          <!-- Vue中借助router-link标签实现路由的切换 -->
-          <!-- router-link本质也是a标签 -->
-          <!-- 完整写法 :replace="true" -->
-          <router-link replace class="list-group-item" active-class="active" to="/about">About</router-link>
-          <router-link replace class="list-group-item" active-class="active" to="/home">Home</router-link>
+					<!-- Vue中借助router-link标签实现路由的切换 -->
+					<router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
         </div>
       </div>
       <div class="col-xs-6">
         <div class="panel">
           <div class="panel-body">
-            <!-- 指定组件的呈现位置 -->
+						<!-- 指定组件的呈现位置 -->
             <router-view></router-view>
           </div>
         </div>
@@ -30,9 +28,9 @@
 </template>
 
 <script>
-  import Banner from './components/Banner.vue'
-  export default {
-    name: "App",
-    components:{Banner}
-  };
+	import Banner from './components/Banner'
+	export default {
+		name:'App',
+		components:{Banner}
+	}
 </script>
