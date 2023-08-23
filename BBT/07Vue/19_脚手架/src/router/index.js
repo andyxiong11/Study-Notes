e14b80496d9745a9790cfaca13bb34e4e51ec1ea
andyxiong11 <xiongyexing1999@163.com>
2023-08-23 22:39:45 +0800

    history模式与hash模式写完

--- BBT/07Vue/19_脚手架/src/router/index.js
+++ BBT/07Vue/19_脚手架/src/router/index.js
@@ -9,6 +9,11 @@ import Detail from '../pages/Detail.vue'
 
 //创建并暴露一个路由器
 const router =  new VueRouter({
+  //#及其后的内容为Hash，如果url是http://localhost:8080/#/，则#及其后的内容不会传给服务器，详见19_src_配置代理服务器中的服务器，用http://localhost:8080/atguigu/students/#/dew打开不会报错
+  //默认为hash，因为在服务器中运行找不到/about路径，详见README.MD文件运行服务器
+  //但是生产不能有#，需要在后端服务器处理，详见README.MD文件
+  mode:history,
+
   routes:[
     {
       name:'about',
