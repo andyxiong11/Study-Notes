8eed1905f913563e8659237a26762bfda19ccb16
andyxiong11 <xiongyexing1999@163.com>
2023-08-23 21:40:56 +0800

    全局路由守卫写完

--- BBT/07Vue/19_脚手架/src/router/index.js
+++ BBT/07Vue/19_脚手架/src/router/index.js
@@ -7,35 +7,41 @@ import News from '../pages/News.vue'
 import Message from '../pages/Message.vue'
 import Detail from '../pages/Detail.vue'
 
-//创建一个路由器
-// const router = new VueRouter({
 //创建并暴露一个路由器
-export default new VueRouter({
+const router =  new VueRouter({
   routes:[
     {
       name:'about',
       path:'/about',
-      component:About
+      component:About,
+      meta:{title:'关于'},
     },
     {
       name:'home',
       path:'/home',
       component:Home,
+      meta:{title:'主页'},
       children:[
         {
           name:'news',
           path:'news',//注意不要斜杠
-          component:News
+          component:News,
+          // 只有meta中可以加属性
+          meta: {isAuth:true,title:'新闻'}
         },
         {
           name:'message',
           path:'message',//注意不要斜杠
           component:Message,
+          // 只有meta中可以加属性
+          meta: {isAuth:true,title:'消息'},
           children:[
             {
               name:'detail',
               path:'detail/:id/:title',//注意不要斜杠
               component:Detail,
+              // 只有meta中可以加属性
+              meta: {isAuth:true,title:'详情'},
 
               //props的第一种写法，值为对象，该对象中的所有key-value都会以props的形式传给Detail组件.
               // props:{a:'1',b:'hello'},
@@ -55,4 +61,36 @@ export default new VueRouter({
   ]
 })
 
-// export default router
+//全局前置路由守卫——初始化的时候被调用、每次路由切换之前被调用
+router.beforeEach((to,from,next)=>{
+  console.log("前置路由守卫",to,from);
+  // 下面的写法，如果鉴权不通过，页签名也会被修改，需要放到afterEach后置路由守卫
+  // document.title = to.meta.title || '光谷系统';
+
+  // next();
+
+  // if (to.path === '/home/news' || to.path === '/home/message') {
+  
+  // 下面是用name替代path
+  // if (to.name === 'news' || to.name === 'message') {
+  
+  // 只有需要校验的路由组件才做判断，鉴权
+  if(to.meta.isAuth){
+    if (localStorage.getItem('school') === 'guanggu') {
+      next()
+    }else{
+      alert('学校名不对，无权限查看')
+    }
+  }else{
+    next()
+  }
+})
+
+//全局后置路由守卫——初始化的时候被调用、每次路由切换之后被调用
+router.afterEach((to,from)=>{
+    console.log("后置路由守卫",to,from);
+    document.title = to.meta.title || '光谷系统';
+  }
+)
+
+export default router
