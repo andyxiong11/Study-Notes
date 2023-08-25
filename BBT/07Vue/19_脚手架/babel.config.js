9740be49f508915ac080662d0bbcf3c7136f5bfd
andyxiong11 <xiongyexing1999@163.com>
2023-08-25 21:03:53 +0800

    element-ui按需引入

--- BBT/07Vue/19_脚手架/babel.config.js
+++ BBT/07Vue/19_脚手架/babel.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+
+    // babel-plugin-component使用
+    //["es2015", { "modules": false }]
+    //上面element-ui官方写法会报错某组件未安装
+    ["@babel/preset-env", { "modules": false }]
+  ],
+  // babel-plugin-component使用
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
--- BBT/07Vue/19_脚手架/src/README.md
+++ BBT/07Vue/19_脚手架/src/README.md
@@ -1,1 +1,2 @@
 1. npm i element-ui
+2. npm install babel-plugin-component -D
