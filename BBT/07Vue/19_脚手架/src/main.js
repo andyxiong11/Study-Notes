ac943e381c7065332a8597764883245b832497a2
andyxiong11 <xiongyexing1999@163.com>
2023-08-25 20:43:26 +0800

    element-ui基本使用

--- BBT/07Vue/19_脚手架/src/main.js
+++ BBT/07Vue/19_脚手架/src/main.js
@@ -2,20 +2,19 @@
 import Vue from "vue";
 //引入App
 import App from './App.vue'
-//引入vue-router
-import VueRouter from 'vue-router'
-//引入路由器
-import router from './router'
+//引入ElementUI组件库
+import ElementUI from 'element-ui';
+//引入ElementUI全部样式
+import 'element-ui/lib/theme-chalk/index.css';
 
 //关闭vue的生产提示
 Vue.config.productionTip = false
 
-//应用插件
-Vue.use(VueRouter)
+//应用ElementUI
+Vue.use(ElementUI);
 
 //创建vm
 new Vue({
   el: '#app',
   render: h => h(App),
-  router: router
 })
