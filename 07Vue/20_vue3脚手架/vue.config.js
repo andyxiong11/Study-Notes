49c6c62d52a57786b98cd8b8ab92b5978b347c84
andyxiong11 <xiongyexing1999@163.com>
2023-08-25 22:08:36 +0800

    分析工程结构学完

--- 07Vue/20_vue3脚手架/vue.config.js
+++ 07Vue/20_vue3脚手架/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+//以下为自定义
+module.exports = {
+  lintOnSave : false,//关闭语法检查
+}
