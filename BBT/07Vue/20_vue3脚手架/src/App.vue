ce392e21878d55e5c1acd782fa93dd4e93421b46
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 17:56:38 +0800

    ref函数处理对象类型

--- BBT/07Vue/20_vue3脚手架/src/App.vue
+++ BBT/07Vue/20_vue3脚手架/src/App.vue
@@ -2,6 +2,8 @@
   <h1>一个人的信息</h1>
   <h2>姓名：{{name}}</h2>
   <h2>年龄：{{age}}</h2>
+  <h3>工作种类:{{ job.type }}</h3>
+  <h3>工作薪水：{{ job.salary }}</h3>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
@@ -15,17 +17,26 @@ export default {
     //数据
     let name = ref('张三')
     let age = ref(18)
+    let job = ref({
+      type: '前端工程师',
+      salary: '30k'
+    })
 
     //方法
     function changeInfo() {
       name.value = '李四';
       age.value = 48;
       // console.log(name,age);
+
+      job.value.type = 'UI设计师';
+      job.value.salary = '60k';  
+      // console.log(job.value);
     }
 
     return {
       name,
       age,
+      job,
       changeInfo,
     }
   }
