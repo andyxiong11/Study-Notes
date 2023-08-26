0b0f78485fee768f6a4e95966cc3059ff939dba5
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 22:00:58 +0800

    vue2 vc中的$attrs和$slots

--- 07Vue/20_vue3脚手架/src/App.vue
+++ 07Vue/20_vue3脚手架/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app">
     <h1>我是Vue2写的效果</h1>
+    <Demo msg="你好啊" school="硅谷">
+      <template slot="test1">
+        <span>硅谷</span>
+      </template>  
+      <template slot="test2">
+        <span>硅谷12</span>
+      </template>
     <Demo/>
   </div>
 </template>
--- 07Vue/20_vue3脚手架/src/components/Demo.vue
+++ 07Vue/20_vue3脚手架/src/components/Demo.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="demo">
-  <h2>我是Demo组件</h2>
+    <h2>我是Demo组件</h2>
+    <!-- {{ $attrs.msg }}
+    {{ $attrs.school }} -->
+    <!-- 上面是不使用props声明接收时使用（未在props中接受的数据都在$attrs中，但不能显示数据类型 -->
+    {{ msg }}
+    {{ school }}
+
+    <!-- <slot></slot> -->
   </div>
 </template>
 <script>
   export default {
-    name: 'Demo'
+    name: 'Demo',
+    props:['msg','school'],
+    mounted() {
+      console.log(this);
+    },
   }
 </script>
 <style>
