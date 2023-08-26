7cc0acf7ded13019fae45fa76f2b28ab59befb65
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 22:12:31 +0800

    初始化

--- 07Vue/20_vue3脚手架/src/App.vue
+++ 07Vue/20_vue3脚手架/src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <h1>一个人的信息</h1>
-  <h2>姓名：{{person.name}}</h2>
-  <h2>年龄：{{person.age}}</h2>
+  <Demo/>
 </template>
 
 <script>
-  import {reactive} from 'vue'
+  import Demo from './components/Demo.vue'
   export default {
     name: 'App',
-    setup() {
-      let person = reactive({
-        name : '张三',
-        age : 18,
-      })
-
-      return {
-        person,
-      }
-    }
+    components: {Demo}
   }
 </script>
