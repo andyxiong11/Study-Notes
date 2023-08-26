fa30b3be3fc9d29d958de98062cc9c935b1b4744
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 19:57:53 +0800

    0825 p114写完

--- 07Vue/20_vue3脚手架/src/App.vue
+++ 07Vue/20_vue3脚手架/src/App.vue
@@ -4,23 +4,46 @@
   <h2>年龄：{{age}}</h2>
   <h3>工作种类:{{ job.type }}</h3>
   <h3>工作薪水：{{ job.salary }}</h3>
+  <h3>爱好：{{ hobby }}</h3>
+  <h3>测试的数据c：{{ job.a.b.c }}</h3>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
 // ref响应式数据
-import {ref} from 'vue'
+import {ref,reactive} from 'vue'
 export default {
   name: 'App',
-  // 此处只是测试一下setup,暂时不考虑响应式的问题
   setup() {
     //数据
     let name = ref('张三')
     let age = ref(18)
-    let job = ref({
+    let job = reactive({
       type: '前端工程师',
-      salary: '30k'
+      salary: '30k',
+      a:{
+        b:{
+          c: 666
+        }
+      }
     })
+    let hobby = reactive(['吃饭','学习','打豆豆'])// 20230806 不用reactive也可以做到数组响应式
+    
+    //可以将所有的数据放到一个对象里面let person = reactive({数据})，return{person}
+    // let person = recative({
+    //   name : ref('张三'),
+    //   age : ref(18),
+    //   job : reactive({
+    //     type: '前端工程师',
+    //     salary: '30k',
+    //     a:{
+    //       b:{
+    //         c: 666
+    //       }
+    //     }
+    //   }),
+    //   hobby : reactive(['吃饭','学习','打豆豆'])// 20230806 不用reactive也可以做到数组响应式
+    // })
 
     //方法
     function changeInfo() {
@@ -28,9 +51,17 @@ export default {
       age.value = 48;
       // console.log(name,age);
 
-      job.value.type = 'UI设计师';
-      job.value.salary = '60 k';  
+      // job.value.type = 'UI设计师';
+      // job.value.salary = '60 k';  
       // console.log(job.value);
+
+      job.type = 'UI设计师';
+      job.salary = '60 k';  
+      // console.log(job);
+
+      job.a.b.c = 999
+
+      hobby[0] = '喝酒'
     }
 
     return {
@@ -38,6 +69,7 @@ export default {
       age,
       job,
       changeInfo,
+      hobby
     }
   }
 }
