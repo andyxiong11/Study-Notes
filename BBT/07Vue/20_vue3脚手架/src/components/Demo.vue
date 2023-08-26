4ed953c4ca21e40a7a6f2927f8ecc718a77bb96f
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 22:12:31 +0800

    初始化

---
+++ BBT/07Vue/20_vue3脚手架/src/components/Demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{person.name}}</h2>
+  <h2>年龄：{{person.age}}</h2>
+</template>
+
+<script>
+  import {reactive} from 'vue'
+  export default {
+    name: 'Demo',
+    setup() {
+      let person = reactive({
+        name : '张三',
+        age : 18,
+      })
+
+      return {
+        person,
+      }
+    }
+  }
+</script>
