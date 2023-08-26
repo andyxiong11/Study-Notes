d5690f948c1b2e57f9152108300c58a12aac365d
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 21:50:23 +0800

    初始化src文件夹

---
+++ 07Vue/20_vue3脚手架/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <h1>我是Vue2写的效果</h1>
+    <Demo/>
+  </div>
+</template>
+<script>
+  import Demo from './components/Demo'
+  export default {
+    name:'App',
+    components:{Demo}
+  }
+</script>
+<style>
+  .app{
+    background-color:gray;
+    padding:10px;
+  }
+</style>  
