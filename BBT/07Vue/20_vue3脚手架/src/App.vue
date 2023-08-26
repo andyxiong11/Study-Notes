bf969dd6609012a9543628bcd41b714d6e03fe0c
andyxiong11 <xiongyexing1999@163.com>
2023-08-26 22:35:14 +0800

    setup的两个注意点

--- BBT/07Vue/20_vue3脚手架/src/App.vue
+++ BBT/07Vue/20_vue3脚手架/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <Demo/>
+  <Demo @hello="showHelloMsg" msg="你好啊" school="硅谷">
+    <!-- <span>硅谷</span> -->
+    <!-- vue3中不建议使用 slot="qwe" -->
+    <template v-slot:qwe>
+      <span>硅谷</span>
+    </template>
+  </Demo>
 </template>
 
 <script>
   import Demo from './components/Demo.vue'
   export default {
     name: 'App',
-    components: {Demo}
+    components: {Demo},
+    setup() {
+      function showHelloMsg(value) {
+        alert(`你好啊，你触发了hello事件，我收到的参数是：${value}!`)
+      }
+      return {
+        showHelloMsg
+      }
+    }
   }
 </script>
