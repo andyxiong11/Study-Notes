4f64f594b0b6ccd662a63088b5fb9f51c8a50dcf
andyxiong11 <xiongyexing1999@163.com>
2023-08-27 22:52:45 +0800

    toRefs写完

--- BBT/07Vue/20_vue3脚手架/src/components/Demo.vue
+++ BBT/07Vue/20_vue3脚手架/src/components/Demo.vue
@@ -2,14 +2,14 @@
   <h4>{{ person }}</h4>
   <h2>姓名：{{name}}</h2>
   <h2>年龄：{{age}}</h2>
-  <h2>薪资：{{ salary }}K</h2>
+  <h2>薪资：{{ job.j1.salary }}K</h2>
   <button @click="name+='~'">修改姓名</button>
   <button @click="age++">增长年龄</button>
-  <button @click="salary++">涨薪</button>
+  <button @click="job.j1.salary++">涨薪</button>
 </template>
 
 <script>  
-  import {ref,reactive,toRef} from 'vue'
+  import {ref,reactive,toRef,toRefs} from 'vue'
   export default {
     name: 'Demo',
 
@@ -33,13 +33,18 @@
       // const name2 = toRef(person,'name')
       // console.log(name2);
 
+      // const x = toRefs(person)
+      // console.log(x);
+
       //返回一个对象（常用）
       return {
         person,
         //响应式写法
-        name: toRef(person,'name'),
-        age: toRef(person,'age'),
-        salary: toRef(person.job.j1,'salary'),
+        // name: toRef(person,'name'),
+        // age: toRef(person,'age'),
+        // salary: toRef(person.job.j1,'salary'),
+
+        ...toRefs(person)
 
         //下面也可以修改展示在页面上的数据，但是person中的原数据没有修改
         // name: ref(person,'name'),
