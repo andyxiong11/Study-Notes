7131d089e773217e354233d9741e55ea2fe57b2b
andyxiong11 <xiongyexing1999@163.com>
2023-08-27 19:30:52 +0800

    初始化src文件夹

---
+++ BBT/07Vue/20_vue3脚手架/src/components/Demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>当前求和为：{{ sum }}</h2>
+  <button @click="sum++">点我+1</button>
+</template>
+
+<script>  
+  import {ref} from 'vue'
+  export default {
+    name: 'Demo',
+    setup() {
+      let sum = ref(0)
+
+      return {
+        sum,
+      }
+    }
+  }
+</script>
