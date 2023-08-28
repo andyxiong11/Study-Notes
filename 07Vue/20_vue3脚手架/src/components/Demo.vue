1f2144318742906d7042c9602dc800e63dfc02b0
andyxiong11 <xiongyexing1999@163.com>
2023-08-28 20:03:21 +0800

    初始化src文件夹

---
+++ 07Vue/20_vue3脚手架/src/components/Demo.vue
@@ -0,0 +1,35 @@
+<template>
+  <h4>{{ person }}</h4>
+  <h2>姓名：{{name}}</h2>
+  <h2>年龄：{{age}}</h2>
+  <h2>薪资：{{ job.j1.salary }}K</h2>
+  <button @click="name+='~'">修改姓名</button>
+  <button @click="age++">增长年龄</button>
+  <button @click="job.j1.salary++">涨薪</button>
+</template>
+
+<script>  
+  import {ref,reactive,toRef,toRefs} from 'vue'
+  export default {
+    name: 'Demo',
+
+    setup() {
+      //数据
+      let person = reactive({
+        name : '张三',
+        age : 18,
+        job:{
+          j1:{
+            salary:20
+          }
+        }
+      })
+
+      //返回一个对象（常用）
+      return {
+        person,
+        ...toRefs(person)
+      }
+    }
+  }
+</script>
