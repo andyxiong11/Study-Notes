28c8aec50cb0d605c257a2c70c5af57476ce31d0
andyxiong11 <xiongyexing1999@163.com>
2023-08-28 22:20:03 +0800

    初始化src文件夹

---
+++ BBT/07Vue/20_vue3脚手架/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>我是App组件</h3>
+</template>
+
+<script>
+  import { ref,reactive,toRefs,readonly } from "vue";
+  export default {
+    name:'App',
+    setup(){
+      let car = reactive({
+        name: '奔驰',
+        price: '40w'
+      })
+      let sum = ref(0)
+      let car2 = readonly(car)
+
+      return {
+        ...toRefs(car)
+      }
+    }
+  }
+</script>
+
+<style>
+  .app{
+    background-color: gray;
+    padding: 10px;
+  }
+</style>
