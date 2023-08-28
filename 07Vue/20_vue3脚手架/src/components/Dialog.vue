f7f43fe7f796f3243ece6fc09730d9cb418f184a
andyxiong11 <xiongyexing1999@163.com>
2023-08-28 22:46:39 +0800

    添加一个新功能组件

---
+++ 07Vue/20_vue3脚手架/src/components/Dialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="isShow = true">点我弹个窗</button>
+    <div v-if="isShow" class="dialog">
+      <h3>我是一个弹窗</h3>
+      <h4>一些内容</h4>
+      <h4>一些内容</h4>
+      <h4>一些内容</h4>
+      <button @click="isShow = false">关闭弹窗</button>
+    </div>
+  </div>
+</template>
+<script>
+  import {ref} from 'vue'
+  export default {
+    name:'Dialog',
+    setup(){
+      let isShow = ref(false)
+      return{
+        isShow
+      }
+    }
+  }
+</script>
+
+<style>
+  .dialog{
+    width:300px;
+    height:300px;
+    background-color:green;
+  }
+</style>
+  
--- 07Vue/20_vue3脚手架/src/components/Son.vue
+++ 07Vue/20_vue3脚手架/src/components/Son.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="son">
     <h3>我是App组件</h3>
+    <Dialog/>
   </div>
 </template>
 
 <script>
+  import Dialog from "../components/Dialog.vue";
   export default {
     name:'Son',
+    components:{Dialog}
   }
 </script>
 
