dd23a266e260a3a3f36674293e83e7ee68635a49
andyxiong11 <xiongyexing1999@163.com>
2023-08-29 22:27:40 +0800

    Suspense组件配合Promise也能实现异步加载

--- BBT/07Vue/20_vue3脚手架/src/components/Child.vue
+++ BBT/07Vue/20_vue3脚手架/src/components/Child.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="child">
     <h3>我是App组件</h3>
+    {{ sum }}
   </div>
 </template>
 
 <script> 
+  import {ref} from 'vue'
   export default {
     name:'Child',
+    setup(){
+      let sum = ref(0)
+      return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+          resolve({sum})
+        }, 3000);
+      })
+    }
   }
 </script>
 
