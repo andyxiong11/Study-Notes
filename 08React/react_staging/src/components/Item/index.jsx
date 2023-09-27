ed7d8d1a629b775e9b22bfc2742ac830e3e1823a
andyxiong11 <xiongyexing1999@163.com>
2023-09-27 22:27:01 +0800

    将一个页面拆分到对应的组件

--- 08React/react_staging/src/components/Item/index.jsx
+++ 08React/react_staging/src/components/Item/index.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import './index.css'
 
 export default class Item extends Component {
   render() {
     return (
-      <div>index</div>
+      <li>
+        <label>
+          <input type="checkbox"/>
+          <span>xxxxx</span>
+        </label>
+        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+      </li>
     )
   }
 }
