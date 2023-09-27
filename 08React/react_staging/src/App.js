1d6d391a8d462b60820b74f9b5ddc428accf98bc
andyxiong11 <xiongyexing1999@163.com>
2023-09-27 21:39:57 +0800

    简写引入需要将index文件修改为.js或者引入index.jsx

--- 08React/react_staging/src/App.js
+++ 08React/react_staging/src/App.js
@@ -1,7 +1,7 @@
 //创建“外壳”组件App
 import React,{Component} from'react' //{Component}不是解构赋值，而是React上有分别暴露Component
-import Hello from './components/Hello' // 引入js和jsx文件都可以忽略文件扩展名
-import Welcome from './components/Welcome' // 引入js和jsx文件都可以忽略文件扩展名
+import Hello from './components/Hello/index.jsx' // 引入js和jsx文件都可以忽略文件扩展名
+import Welcome from './components/Welcome/index.jsx' // 引入js和jsx文件都可以忽略文件扩展名
 
 //创建并暴露App组件
 //class App extends React.Component{
