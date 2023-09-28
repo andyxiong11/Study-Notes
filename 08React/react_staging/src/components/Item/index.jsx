968ad0875aafc831156f484475082202372a9071
[andyxiong11] <[xiongyexing1999@163.com]>
2023-09-28 22:26:49 +0800

    增加鼠标移入效果

--- 08React/react_staging/src/components/Item/index.jsx
+++ 08React/react_staging/src/components/Item/index.jsx
@@ -2,17 +2,27 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+
+  state = {mouse:false}
+
+  handleMouse = (flag) => {
+    return ()=>{// 调用时传了参数，返回值必须是函数
+      this.setState = ({mouse:flag})
+    }
+  }
   render() {
-    const {id,name,done} = this.props
+    const {name,done} = this.props
+    const {mouse} = this.state
     return (
-      <li>
+      // onMouseEnter鼠标移入事件 onmouseleave鼠标移出事件
+      <li style={{backgroundColor:mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         <label>
           {/* <input type="checkbox" checked={done}/> */}
           {/* 使用defaultChecked 可以修改默认勾选 */}
           <input type="checkbox" defaultChecked={done}/>
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+        <button className="btn btn-danger" style={{display:mouse?'block':'none'}}>删除</button>
       </li>
     )
   }
