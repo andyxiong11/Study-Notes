ba6dbecc3395628c0b019a11b6bcefd605710b85
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-07 22:24:44 +0800

    旧版本配置代理方法2

--- 08React/react_staging/src/App.jsx
+++ 08React/react_staging/src/App.jsx
@@ -5,7 +5,14 @@ import axios from 'axios'
 export default class App extends Component {
   getStudentData = ()=>{
     // 在package.json配置代理后就不能发到端口为5000，需要发到3000，不然还是跨域。且优先会在本地寻找是否存在该路径
-    axios.get('http://localhost:3000/students').then(
+    axios.get('http://localhost:3000/api1/students').then(
+      response => {console.log('成功了',response.data);},
+      error => {console.log('失败了',error);}
+    )
+  }
+  getCarData = ()=>{
+    // 在package.json配置代理后就不能发到端口为5000，需要发到3000，不然还是跨域。且优先会在本地寻找是否存在该路径
+    axios.get('http://localhost:3000/api2/cars').then(
       response => {console.log('成功了',response.data);},
       error => {console.log('失败了',error);}
     )
@@ -14,6 +21,7 @@ export default class App extends Component {
     return (
       <div>
         <button onClick={this.getStudentData}>点我获取学生数据</button>
+        <button onClick={this.getCarData}>点我获取汽车数据</button>
       </div>
     )
   }
