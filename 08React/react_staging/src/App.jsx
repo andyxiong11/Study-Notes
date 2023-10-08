f5a0e2ba538e0a23de84fada3e7659f6773fa0d2
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-08 22:12:38 +0800

    消息订阅与发布，优化github搜索案例

--- 08React/react_staging/src/App.jsx
+++ 08React/react_staging/src/App.jsx
@@ -4,31 +4,11 @@ import List from "./components/List";
 
 export default class App extends Component {
 
-  state = { //初始化状态
-    users:[],
-    isFarst:true,//是否第一次打开页面
-    isLoading:false,//是否处于加载中
-    err:''//存储请求的错误信息
-  }
-
-  // saveUsers = (users)=>{
-  //   this.setState({users:users})
-  // }
-
-  // 优化添加各种效果
-  updateAppState = (stateObj)=>{
-    this.setState(stateObj)
-  }
-
   render() {
     return (
       <div className="container">
-        {/* <Search saveUsers={this.saveUsers}/> */}
-        <Search updateAppState={this.updateAppState}/>
-
-        {/* <List users={this.state.users}/> */}
-        {/* <List users={users} isFarst={isFarst}/> */}
-        <List {...this.state}/>
+        <Search/>
+        <List/>
       </div>
     )
   }
