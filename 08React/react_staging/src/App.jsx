75a1b775a4cac9c1759ee384c5beb888a632869f
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-08 21:16:36 +0800

    github搜索案例基本完成

--- 08React/react_staging/src/App.jsx
+++ 08React/react_staging/src/App.jsx
@@ -3,11 +3,18 @@ import Search from "./components/Search";
 import List from "./components/List";
 
 export default class App extends Component {
+
+  state = {users:[]}//初始化状态
+
+  saveUsers = (users)=>{
+    this.setState({users:users})
+  }
+
   render() {
     return (
       <div className="container">
-        <Search/>
-        <List/>
+        <Search saveUsers={this.saveUsers}/>
+        <List users={this.state.users}/>
       </div>
     )
   }
--- 08React/react_staging/src/components/Search/index.jsx
+++ 08React/react_staging/src/components/Search/index.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Search extends Component {
+  search = ()=>{
+    //获取用户的输入
+    //const {value} = this.keyWordElement
+    // 下面是连续解构赋值+重命名
+    const {keyWordElement:{value:keyWord}} = this //简写
+    //console.log(keyWordElement);//报错
+    console.log(keyWord);
+    //发送网络请求 github后端有cors，所以不会产生跨域
+    //axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+    //axios.get(`http://localhost:5000/search/users?q=${keyWord}`).then(//使用代理服务器，会产生跨域
+    //axios.get(`http://localhost:3000/api1/search/users?q=${keyWord}`).then(//配置setupProxy.js代理避免跨域
+    axios.get(`/api1/search/users?q=${keyWord}`).then(//本地服务器可以简写
+      response => {
+        //console.log('成功了',response.data);
+        this.props.saveUsers(response.data.items)
+      },
+      error => {console.log('失败了',error);}
+    )
+  }
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">搜索github用户</h3>
         <div>
-          <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+          <input ref={c => this.keyWordElement = c} type="text" placeholder="输入关键词点击搜索"/>&nbsp;
+          <button onClick={this.search}>搜索</button>
         </div>
       </section>
     )
