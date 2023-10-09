869e202ccf23a8b69c438f0db4e4f52cefc6569f
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-09 23:05:08 +0800

    路由组件与一般组件，NavLink的使用.1009 p79结束

--- BBT/08React/react_staging/src/App.jsx
+++ BBT/08React/react_staging/src/App.jsx
@@ -1,8 +1,9 @@
 //安装ES7+ React/Redux/React-Native 插件，使用rcc、rfc快速生成代码片段 
 import React, { Component } from 'react'
-import { Link,BrowserRouter,Route } from "react-router-dom";
-import Home from './components/Home'
-import About from './components/About'
+import { Link,BrowserRouter,Route,NavLink } from "react-router-dom";
+import Home from './pages/Home'//路由组件
+import About from './pages/About'//路由组件
+import Header from "./components/Header"//一般组件
 
 export default class App extends Component {
 
@@ -11,14 +12,17 @@ export default class App extends Component {
       <div>
         <div className="row">
           <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header"><h2>React Router Demo</h2></div>
+            <Header/>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
-              <Link className="list-group-item active" to="/about">About</Link>
-              <Link className="list-group-item" to="/home">Home</Link>
+              {/* 刚好NavLink组件的作用默认是点击就加一个active样式，而bootstrap也有一个active样式 */}
+              {/* <NavLink className="list-group-item" to="/about">About</NavLink>
+              <NavLink className="list-group-item" to="/home">Home</NavLink> */}
+              <NavLink activeClassName="atguanggu" className="list-group-item" to="/about">About</NavLink>
+              <NavLink activeClassName="atguanggu" className="list-group-item" to="/home">Home</NavLink>
             </div>
           </div>
           <div className="col-xs-6">
