d5716e892944d449407787c7db3a29c42518c371
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-14 21:06:24 +0800

    向路由组件传递search参数

--- 08React/react_staging/src/pages/Home/Message/Detail/index.jsx
+++ 08React/react_staging/src/pages/Home/Message/Detail/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import qs from 'qs' //也可以安装querysrting npm i -save-dev query-srting，import QueryString from 'query-string'
 
 const DetailData = [
   {id:'001',content:'消息1'},
@@ -9,7 +10,12 @@ export default class Detail extends Component {
   render() {
     console.log(this.props);
     // 接收params参数
-    const {id,title} = this.props.match.params
+    // const {id,title} = this.props.match.params 
+
+    //接收search参数
+    const {search} = this.props.location
+    const {id,title} = qs.parse(search.slice(1))//.slice为了去除search前面的“？”；qs.parse将search分割为对象
+
     const findResult = DetailData.find((detailObj)=>{
       return detailObj.id === id
     })
