8471edce702cde68d443b6fea32c797cd5f757e0
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-14 22:47:12 +0800

    withRouter使用

--- BBT/08React/react_staging/src/components/Header/index.jsx
+++ BBT/08React/react_staging/src/components/Header/index.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import {withRouter} from 'react-router-dom'
 
-export default class Header extends Component {
+class Header extends Component {
+  back = ()=>{
+    this.props.history.goBack()
+  }
+
+  forward = ()=>{
+    this.props.history.goForward()
+  }
+
+  go = ()=>{
+    this.props.history.go(2)
+  }
   render() {
     console.log("Header收到的props",this.props);
     return (
-      <div className="page-header"><h2>React Router Demo</h2></div>
+      <div className="page-header">
+        <h2>React Router Demo</h2>
+        <button onClick={this.back}>回退</button>
+        <button onClick={this.forward}>前进</button>
+        <button onClick={this.go}>前进2步</button>
+      </div>
     )
   }
 }
+
+export default withRouter(Header)//将Header组件经过withRouter加工后暴露
+//withRouter可以加工一般组件，让一般组件具备路由组件所特有的API
+//withRouter的返回值是一个新组件
