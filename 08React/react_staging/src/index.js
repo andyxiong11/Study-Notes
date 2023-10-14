e5ab5fb5c3ab589ab572fc1a6eaaec820230c82e
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-14 22:45:05 +0800

    初始化src

---
+++ 08React/react_staging/src/index.js
@@ -0,0 +1,15 @@
+//引入react核心库
+import React from 'react'
+//引入ReactDOM
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from "react-router-dom";
+//引入App组件
+import App from './App'
+
+//渲染APP到页面
+ReactDOM.render(
+  // 如果是使用HashRouter,会多出一个#，且#后面的内容不会发送给服务器
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+  ,document.getElementById('root'))
