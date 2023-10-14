00fc4db508d1c85dff0edc47a19ec4ee9ffc2667
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-14 22:08:01 +0800

    初始化src

---
+++ 08React/react_staging/src/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+// import qs from 'qs' //也可以安装querysrting npm i -save-dev query-srting，import QueryString from 'query-string'
+
+const DetailData = [
+  {id:'001',content:'消息1'},
+  {id:'002',content:'消息2'},
+  {id:'003',content:'消息3'},
+]
+export default class Detail extends Component {
+  render() {
+    console.log(this.props);
+    // 接收params参数
+    // const {id,title} = this.props.match.params 
+
+    //接收search参数
+    // const {search} = this.props.location
+    // const {id,title} = qs.parse(search.slice(1))//.slice为了去除search前面的“？”；qs.parse将search分割为对象
+
+    // 接收search参数
+    const {id,title} = this.props.location.state || {} // || {} 避免在没有历史记录缓存的情况下直接访问“http://localhost:3000/home/message/detail?id=001&title=消息1” 报错
+
+    const findResult = DetailData.find((detailObj)=>{
+      return detailObj.id === id
+    }) || {}
+    return (
+      <ul>
+        <li>
+          ID:{id}
+        </li>
+        <li>
+          TITLE:{title}
+        </li>
+        <li>
+          CONTENT:{findResult.content}
+        </li>
+      </ul>
+    )
+  }
+}
---
+++ 08React/react_staging/src/pages/Home/Message/index.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {Link,Route} from 'react-router-dom'
+import Detail from './Detail'
+
+export default class Message extends Component {
+  state = {
+    messageArr:[
+      {id:'001',title:'消息1'},
+      {id:'002',title:'消息2'},
+      {id:'003',title:'消息3'},
+    ]
+  }
+  render() {
+    const {messageArr} = this.state
+    return (
+      <div>
+        <ul>
+          {
+            messageArr.map((msgObj)=>{
+              return (
+                <li key={msgObj.id}>
+                  {/* 向路由组件传递params参数 */}
+                  {/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link> */}
+                
+                  {/* 向路由组件传递search参数 */}
+                  {/* <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link> */}
+
+                  {/* 向路由组件传递state参数 */}
+                  {/* replace模式 每次都会替换浏览器历史记录栈顶的历史记录 无法返回上个页面；默认是push模式 */}
+                  <Link replace={false} to={{pathname:'/home/message/detail',state:{id:msgObj.id,title:msgObj.title}}}>{msgObj.title}</Link>
+
+                </li>
+              )
+            })
+          }
+        </ul>
+        <hr/>
+        {/* 声明接收params参数 */}
+        {/* <Route path="/home/message/detail/:id/:title" component={Detail} /> */}
+
+        {/* search参数无需声明接收 */}
+        {/* <Route path="/home/message/detail" component={Detail} /> */}
+
+        {/* state参数无需声明接收 */}
+        <Route path="/home/message/detail" component={Detail} />
+
+      </div>
+    )
+  }
+}
