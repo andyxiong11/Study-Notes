384745d9fba1e3b475466982559d43641f9d99f7
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-15 21:25:40 +0800

    antd基本使用

--- BBT/08React/react_staging/src/App.jsx
+++ BBT/08React/react_staging/src/App.jsx
@@ -1,12 +1,26 @@
 //安装ES7+ React/Redux/React-Native 插件，使用rcc、rfc快速生成代码片段 
 import React, { Component } from 'react'
+import { Button,DatePicker } from 'antd'
+// import 'antd/dist/reset.css' //antd5不需要引入css样式表
+import { WechatOutlined,WeiboOutlined,SearchOutlined } from '@ant-design/icons' //antd图标库
+const { RangePicker } = DatePicker
 
 export default class App extends Component {
-
   render() {
     return (
       <div>
         App...
+        <button>点击</button>
+        <Button type="primary">按钮1</Button>
+        <Button>按钮2</Button>
+        <Button type="link">按钮3</Button>
+        <Button type="primary" icon={<SearchOutlined/>}>
+          Search
+        </Button>
+        <WechatOutlined/>
+        <WeiboOutlined/>
+        <DatePicker/>
+        <RangePicker/>
       </div>  
     )
   }
