7220c9f64ce3622925e24640a14e8c0d7fa20758
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-15 20:14:13 +0800

    初始化src

---
+++ 08React/react_staging/src/App.jsx
@@ -0,0 +1,13 @@
+//安装ES7+ React/Redux/React-Native 插件，使用rcc、rfc快速生成代码片段 
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        App...
+      </div>  
+    )
+  }
+}
