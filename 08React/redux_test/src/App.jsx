02716d2af0f7b19b2017b85d377db2e6e36c45ac
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-18 21:38:52 +0800

    使用Provider组件优化

--- 08React/redux_test/src/App.jsx
+++ 08React/redux_test/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import store from './redux/store'
+// import store from './redux/store'
 import Count from './containers/Count'
 
 export default class App extends Component {
@@ -7,7 +7,9 @@ export default class App extends Component {
     return (
       <div>
         {/* 给容器组件传递store */}
-        <Count store={store}/>
+        {/* <Count store={store}/> */}
+        {/* index.js中Provider会给App组件下面的所有组件传递store，无需在每个组件传递store */}
+        <Count/> 
       </div>
     )
   }
