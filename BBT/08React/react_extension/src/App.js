8f65b8c91f4bfb4f0381e4223bea7c4006cdbe96
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-22 21:52:02 +0800

    初始化src

--- BBT/08React/react_extension/src/App.js
+++ BBT/08React/react_extension/src/App.js
@@ -5,7 +5,8 @@ import React, { Component } from 'react'
 // import Demo from './components/4_Fragment'
 // import Demo from './components/5_Context'
 // import Demo from './components/6_optimize'
-import Demo from './components/7_renderProps'
+// import Demo from './components/7_renderProps'
+import Demo from './components/8_ErrorBounday/Parent'
 
 export default class App extends Component {
   render() {
