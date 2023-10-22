aa741fb4ca73ecc8ff32d562b6950917291d1278
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-22 22:47:50 +0800

    初始化src

---
+++ 08React/react_router_6/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {NavLink,Routes,Route} from 'react-router-dom' //新版本Routes替代Switch
+import About from './pages/About'
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header"><h2>React Router Demo</h2></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/* 路由链接 */}
+            <NavLink className="list-group-item" to="/about.html">About</NavLink>
+            <NavLink className="list-group-item active" to="/home.html">Home</NavLink>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+              {/* 注册路由 */}
+              {/* 新版本Routes替代Switch */}
+              <Routes>
+                {/* 新版本element替代component */}
+                <Route path="/about" element={<About/>}/>
+                <Route path="/home" element={<Home/>}/>
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
