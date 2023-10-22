5e4b9f79c1a344f778b7a97d8daf904e62503461
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-22 20:37:31 +0800

    继续PureComponent写完

--- 08React/react_extension/src/components/6_optimize/index.jsx
+++ 08React/react_extension/src/components/6_optimize/index.jsx
@@ -1,21 +1,40 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React, { PureComponent } from 'react' //PureComponent组件，相当于shouldComponentUpdate
 import './index.css'
 
-export default class Parent extends Component {
+// export default class Parent extends Component {
+export default class Parent extends PureComponent {
 
-  state = {carName:"奔驰"}
+
+  state = {carName:"奔驰",stus:['小郑','小李','小王']}
+
+  addStu = ()=>{
+    /* const {stus} = this.state
+    stus.unshift('小刘')
+    this.setState({stus}) */
+    // 因为上面的写法没有更改存储地址，所以PureComponent无法检测到
+
+    const {stus} = this.state
+    this.setState({stus:['小刘',...stus]})
+  }
 
   changeCar = ()=>{
     this.setState({carName:'迈巴赫'})
     // this.setState({})//即使不修改状态也会render
+
+    // 因为下面的写法没有更改存储地址，所以PureComponent无法检测到
+    /* const obj = this.state
+    obj.carName = '迈巴赫'
+    this.setState(obj) */
   }
 
-  shouldComponentUpdate(nextProps,nextState){//shouldComponentUpdate控制组件更新
+  /* shouldComponentUpdate(nextProps,nextState){//shouldComponentUpdate控制组件更新
     console.log(this.props,this.state); //目前的props和state
     console.log(nextProps,nextState);//接下来要变化的目标props和state
-    if(this.state.carName === nextState.carName) return false
-    else return true
-  }
+    //if(this.state.carName === nextState.carName) return false //如果状态没有变化时不刷新组件
+    //else return true
+    return !(this.state.carName === nextState.carNam)
+  } */
 
   render() {
     console.log("Parent---render");
@@ -23,21 +42,24 @@ export default class Parent extends Component {
     return (
       <div className="parent">
         <h3>我是Parent组件</h3>
+        {this.state.stus}&nbsp;
         <span>我的车名字是：{carName}</span>
         <button onClick={this.changeCar}>点我换车</button>
-        <Child carName={carName}/>
+        <button onClick={this.addStu}>点我增加小刘</button>
+        <Child carName='奥托'/>
       </div>
     )
   }
 }
 
-class Child extends Component {
-  shouldComponentUpdate(nextProps,nextState){//shouldComponentUpdate控制组件更新
+class Child extends PureComponent {
+  /* shouldComponentUpdate(nextProps,nextState){//shouldComponentUpdate控制组件更新
     console.log(this.props,this.state); //目前的props和state
     console.log(nextProps,nextState);//接下来要变化的目标props和state
-    if(this.state.carName === nextState.carName) return false
-    else return true
-  }
+    //if(this.props.carName === nextProps.carName) return false //如果状态没有变化时不刷新组件
+    //else return true
+    return !(this.props.carName === nextProps.carName)
+  } */
 
   render() {
     console.log("Child---render");
