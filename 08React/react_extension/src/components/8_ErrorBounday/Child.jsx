20f542b30582763521d5eaef9d3d58f6cf86ba3a
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-22 21:52:02 +0800

    初始化src

---
+++ 08React/react_extension/src/components/8_ErrorBounday/Child.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Child extends Component {
+  state = {
+    user:[
+      {id:'001',name:'tom',age:18},
+      {id:'002',name:'jack',age:19},
+      {id:'003',name:'tom',age:20},
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <h2>我是Child组件</h2>
+        {
+          this.state.user.map((uesrObj)=>{
+            return <h4 key={uesrObj.id}>{uesrObj.name}---{uesrObj.age}</h4>
+          })
+        }
+      </div>
+    )
+  }
+}
