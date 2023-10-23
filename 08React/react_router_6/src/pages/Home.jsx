5f1d991da0432bab993d477ac89a17a8ae962e84
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-23 21:25:46 +0800

    初始化src

---
+++ 08React/react_router_6/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink,Outlet } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home组件内容</h2>
+      <div>
+        <ul className="nav nav-tabs">
+          <li>
+            {/* 可以去掉/home/ */}
+            <NavLink className="list-group-item" to="news">News</NavLink>
+          </li>
+          <li>
+            {/* 可以去掉/home/ */}
+            <NavLink className="list-group-item" to="message">Message</NavLink>
+          </li>
+        </ul>
+        {/* 指定路由组件的呈现位置 */}
+        <Outlet/>
+      </div>
+    </div>
+  )
+}
