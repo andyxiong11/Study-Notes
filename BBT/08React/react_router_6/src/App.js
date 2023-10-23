727f0ed177d21e223cd52a2382f0ecf303726886
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-23 20:17:22 +0800

    NavLink高亮

--- BBT/08React/react_router_6/src/App.js
+++ BBT/08React/react_router_6/src/App.js
@@ -4,6 +4,9 @@ import About from './pages/About'
 import Home from './pages/Home'
 
 export default function App() {
+  function computedClassName({isActive}) {
+    return isActive ? "list-group-item atguanggu" : "list-group-item"
+  }
   return (
     <div>
       <div className="row">
@@ -15,8 +18,12 @@ export default function App() {
         <div className="col-xs-2 col-xs-offset-2">
           <div className="list-group">
             {/* 路由链接 */}
-            <NavLink className="list-group-item" to="/about">About</NavLink>
-            <NavLink className="list-group-item active" to="/home">Home</NavLink>
+            {/* <NavLink activeClassNmae = "atguanggu" className="list-group-item" to="/about">About</NavLink> */}
+            {/* 上面是reactRoute5的写法 */}
+            {/* reactRoute6使用函数，a是一个对象，其中有isActive属性，路由匹配上则为true */}
+            {/* <NavLink className={(a)=>{console.log(a);}} to="/about">About</NavLink> */}
+            <NavLink className={(isActive)=> isActive ? "list-group-item atguanggu" : "list-group-item"} to="/about">About</NavLink>
+            <NavLink className={computedClassName} to="/home">Home</NavLink>
           </div>
         </div>
         <div className="col-xs-6">
--- BBT/08React/react_router_6/src/pages/Home.jsx
+++ BBT/08React/react_router_6/src/pages/Home.jsx
@@ -1,15 +1,9 @@
 import React,{useState} from 'react'
-import {Navigate} from 'react-router-dom'
 
 export default function Home() {
-  const [sum,setSum] = useState(1)//sum初始值为1，修改sum的方法是setSum
   return (
     <div>
       <h3>我是Home的内容</h3>
-      {/* replace 控制是否浏览器可以返回(历史记录push模式) */}
-      {sum === 2 ? <Navigate to="/about" replace={true}/> : <h4>当前的sum值是：{sum}</h4>}
-      {/* 防止函数立即执行，写成回调函数 */}
-      <button onClick={()=>setSum(2)}>点我将sum变为2</button>
     </div>
   )
 }
