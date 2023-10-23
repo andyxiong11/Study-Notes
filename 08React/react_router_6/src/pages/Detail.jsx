0f5e6c86a72ea860f1f1880c72641103de13169c
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-23 21:59:06 +0800

    路由的search参数

--- 08React/react_router_6/src/pages/Detail.jsx
+++ 08React/react_router_6/src/pages/Detail.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { useParams,useMatch } from 'react-router-dom'
+import {useSearchParams,useLocation} from 'react-router-dom'
 
 export default function Detail() {
-  const {id,title,content} = useParams()//useParams返回父组件传的所有参数
-  const x = useMatch('/home/message/detail/:id/:title/:content')
-  console.log(x.params);//useMatch相当于类式组件的this.props.match.params，用起来不如useParams方便
+  const [search,setSearch] = useSearchParams()//useSearchParams返回一个数组，类似setState
+  const id = search.get('id')//通过get方法取传参
+  const title = search.get('title')//通过get方法取传参
+  const content = search.get('content')//通过get方法取传参
+  const x = useLocation()
+  console.log(x);//useLocation相当于类式组件的this.props.location.searchs，用起来不如useSearchParams方便
   return (
     <div>
       <ul>
+        <li>
+          {/* setSearch更新search */}
+          <button onClick={()=>setSearch('id=001&title=消息1&content=锄禾日当午')}>点我更新收到的参数</button>
+        </li>
         <li>消息编号：{id}</li>
         <li>消息标题：{title}</li>
         <li>消息内容：{content}</li>
