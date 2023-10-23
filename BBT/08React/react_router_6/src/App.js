f0efc6c674cec30f5b6b464ef4c828973d0cb7f4
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-23 21:03:54 +0800

    嵌套路由

--- BBT/08React/react_router_6/src/App.js
+++ BBT/08React/react_router_6/src/App.js
@@ -17,7 +17,8 @@ export default function App() {
           <div className="list-group">
             {/* 路由链接 */}
             <NavLink className="list-group-item" to="/about">About</NavLink>
-            <NavLink className="list-group-item" to="/home">Home</NavLink>
+            {/* end 子级路由匹配上，父级路由不高亮 */}
+            <NavLink className="list-group-item" end to="/home">Home</NavLink>
           </div>
         </div>
         <div className="col-xs-6">
