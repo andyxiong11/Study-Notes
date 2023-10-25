0c6a4e4c80b9a7b1f4b0854d144fac234791b4d3
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-25 21:51:07 +0800

    text组件基本使用

--- 09 uni-app/hello-uni/pages/detail/detail.vue
+++ 09 uni-app/hello-uni/pages/detail/detail.vue
@@ -1,5 +1,30 @@
 <template>
-	<view>详情页</view>
+	<view>
+		<view>
+			<text>唱歌跳舞篮球</text>
+		</view>
+		<view>
+			<!-- selectable 文字是否可选 -->
+			<text selectable>唱歌跳舞篮球</text>
+		</view>
+		<view>
+			<!-- space 显示连续空格(钉钉小程序不支持) -->
+			<!-- ensp  中文字符空格一半大小-->
+			<text selectable space="ensp">唱歌    跳舞篮球</text>
+		</view>
+		<view>
+			<!-- emsp  中文字符空格大小-->
+			<text selectable space="emsp">唱歌    跳舞篮球</text>
+		</view>
+		<view>
+			<!-- nbsp  根据字体设置的空格大小-->
+			<text selectable space="nbsp" style="font-style: 100px;">唱歌    跳舞篮球</text>
+		</view>
+		<view>
+			<!-- decode 是否解码（百度、钉钉小程序不支持）-->
+			<text decode>&amp;</text>
+		</view>
+	</view>
 </template>
 
 <script>
