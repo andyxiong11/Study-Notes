29f35ef55cb025ac328cae48f052805b950810ef
[andyxiong11] <[xiongyexing1999@163.com]>
2023-10-30 20:41:09 +0800

    数据缓存

--- 09 uni-app/hello-uni/pages/list/list.vue
+++ 09 uni-app/hello-uni/pages/list/list.vue
@@ -1,6 +1,9 @@
 <template>
 	<view>
 		<button @click="get">发送get请求</button>
+		<button type="primary" @click="setStorage">存储数据</button>
+		<button type="primary" @click="getStorage">获取数据</button>
+		<button type="primary" @click="removeId">移除id数据</button>
 		<view>这是列表页</view>
 		<view class="box-item" v-for="item in list">
 			{{item}}
@@ -43,6 +46,35 @@
 							console.log(res);
 						}
 				})
+			},
+			setStorage(){
+				/* uni.setStorage({//异步存储
+					key:'id',
+					data:80,
+					success() {
+						console.log('存储成功');
+					}
+				}) */
+				uni.setStorageSync('id',80)//同步存储
+			},
+			getStorage(){
+				/* uni.getStorage({//异步获取
+					key:'id',
+					success(res) {
+						console.log('获取成功',res.data);
+					}
+				}) */
+				const res = uni.getStorageSync('id');//同步获取
+				console.log(res);
+			},
+			removeId(){
+				/* uni.removeStorage({//异步删除
+					key:'id',
+					success(res) {
+						console.log('移除成功');
+					}
+				}) */
+				uni.removeStorageSync('id')//同步删除
 			}
 		}
 	}
