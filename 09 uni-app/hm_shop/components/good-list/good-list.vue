ba884d654e7d3e8613beb2c63529adef1e0f30bd
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-01 22:58:33 +0800

    封装商品列表组件及使用，1101 p38结束，预计p40

---
+++ 09 uni-app/hm_shop/components/good-list/good-list.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="goods_list">
+		<view class="goods_item" v-for="item in goods" :key="item.id">
+			<image :src="item.img_url"></image>
+			<view class="price">
+				<text>￥{{item.sell_price}}</text>
+				<text>￥{{item.market_price}}</text>
+			</view>
+			<view class="name">
+				{{item.title}}
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:['goods']
+	}
+</script>
+
+<style lang="scss">
+	.goods_list{
+		padding:0 15rpx;
+		display:flex;
+		flex-wrap:wrap;//换行
+		justify-content:space-between;//两端对齐
+		.goods_item{
+			background:#fff;
+			width:355rpx;
+			margin:10rpx 0;
+			padding: 15rpx;
+			box-sizing: border-box;//边框和内边距的值包含在 width 内
+			image{
+				width:80%;
+				height:150px;
+				display: block;//块级对象，方便margin: auto
+				margin: auto;
+				background-color: pink;//图片打不开，模拟图片
+			}
+		}
+	}
+	.price{
+		color:$shop-color;
+		font-size:36rpx;
+		max-width: 20rpx 0 10rpx 0;
+		text:nth-child(2){
+			color:#ccc;
+			font-size:28rpx;
+			margin-left:17rpx;
+			text-decoration:line-through;//文本的中间将显示一条线
+		}
+	}
+	.name{
+		font-size:28rpx;
+		line-height:50rpx;
+		padding-bottom:15rpx;
+		padding-top:10rp×;
+	}
+</style>
--- 09 uni-app/hm_shop/pages/goods/goods.vue
+++ 09 uni-app/hm_shop/pages/goods/goods.vue
@@ -1,22 +1,36 @@
 <template>
-	<view>
-		商品列表
+	<view class="goods_list">
+		<good-list :goods="goods"/>
 	</view>
 </template>
 
 <script>
+	import goodList from '../../components/good-list/good-list.vue'
 	export default {
 		data() {
 			return {
-				
+				goods:[]//热门商品
 			}
 		},
+		onLoad() {
+			this.getHotGoods()//获取热门商品列表数据
+		},
 		methods: {
-			
-		}
+			// 获取热门商品列表数据
+			async getHotGoods(){
+				const res = await this.$myRequest({
+					url:'/api/getgoods?pageindex=1',
+				})
+				console.log(res);
+				this.goods = res.data.message
+			},
+		},
+		components:{"good-list":goodList}
 	}
 </script>
 
 <style>
-
+	.goods_list{
+		background-color: #eee;
+	}
 </style>
