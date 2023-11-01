2b4efe43c54861ca0d319271e493bf4091905a3a
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-01 22:58:33 +0800

    封装商品列表组件及使用，1101 p38结束，预计p40

--- BBT/09 uni-app/hm_shop/pages/index/index.vue
+++ BBT/09 uni-app/hm_shop/pages/index/index.vue
@@ -18,23 +18,13 @@
 		<!-- 推荐商品 -->
 		<view class="hot_goods">
 			<view class="tit">推荐商品</view>
-			<view class="goods_list">
-				<view class="goods_item" v-for="item in goods" :key="item.id">
-					<image :src="item.img_url"></image>
-					<view class="price">
-						<text>￥{{item.sell_price}}</text>
-						<text>￥{{item.market_price}}</text>
-					</view>
-					<view class="name">
-						{{item.title}}
-					</view>
-				</view>
-			</view>
+			<good-list :goods="goods"/>
 		</view>
 	</view>
 </template>
 
 <script>
+	import goodList from '../../components/good-list/good-list.vue'
 	export default {
 		data() {
 			return {
@@ -68,6 +58,7 @@
 			this.getSwipers(),//获取轮播图的数据
 			this.getHotGoods()//获取热门商品列表数据
 		},
+		components:{"good-list":goodList},
 		methods: {
 			//TODO 小程序渲染轮播图失败
 			// 获取轮播图的数据
@@ -160,41 +151,5 @@
 		// 	margin: 7px 0; 等同于 padding: 10px 0;
 		}
 	}
-	.goods_list{
-		padding:0 15rpx;
-		display:flex;
-		flex-wrap:wrap;//换行
-		justify-content:space-between;//两端对齐
-		.goods_item{
-			background:#fff;
-			width:355rpx;
-			margin:10rpx 0;
-			padding: 15rpx;
-			box-sizing: border-box;//边框和内边距的值包含在 width 内
-			image{
-				width:80%;
-				height:150px;
-				display: block;//块级对象，方便margin: auto
-				margin: auto;
-				background-color: pink;//图片打不开，模拟图片
-			}
-		}
-	}
-	.price{
-		color:$shop-color;
-		font-size:36rpx;
-		max-width: 20rpx 0 10rpx 0;
-		text:nth-child(2){
-			color:#ccc;
-			font-size:28rpx;
-			margin-left:17rpx;
-			text-decoration:line-through;//文本的中间将显示一条线
-		}
-	}
-	.name{
-		font-size:28rpx;
-		line-height:50rpx;
-		padding-bottom:15rpx;
-		padding-top:10rp×;
-	}
+	
 </style>
