aaa5a4aaf3f9fb74010313d0ba097982cce80023
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-02 22:37:10 +0800

    联系我们页面，1101结束

---
+++ BBT/09 uni-app/hm_shop/pages/contact/contact.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="contact">
+		<image src="http://www.itcast.cn/2018czydz/images/gywmban.jpg" class="img"></image>
+		<view class="info">
+			<view @click="phone">联系电话：400-618-9090（点击拨打）</view>
+			<view>校区地址：浙江省杭州市下沙经济开发区4号大街187号盛泰时代山</view>
+		</view>
+		<!-- 地图组件 -->
+		<map class="map" :longitude="longitude" :scale="scale" :latitude="latitude" :markers="markers"></map>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				// 经纬度
+				longitude:"120",
+				latitude:"30",
+				// 标记点
+				markers:[
+					{
+						longitude:"120",
+						latitude:"30",
+						iconPath:"../../static/hmlogo.png",//图片
+						with:30,
+						height:30
+					}
+				],
+				//缩放
+				scale:13
+			}
+		},
+		methods: {
+			phone(){
+				uni.makePhoneCall({
+					phoneNumber:"4006189090"
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.contact{
+		.img{
+			width: 750rpx;
+			height: 320rpx;
+		}
+		.info{
+			padding: 10rpx 20rpx;
+			font-size: 30rpx
+			view{
+				line-height: 80rpx;
+				border-bottom: 1px solid #eee;
+			}
+		}
+		.map{
+			width: 750rpx;
+			height: 750rpx;
+		}
+	}
+</style>
