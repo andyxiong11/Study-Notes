cb8e172aedef98bb238a37be229c997a3171ea96
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-03 22:43:04 +0800

    资讯详情完成

--- 09 uni-app/hm_shop/main.js
+++ 09 uni-app/hm_shop/main.js
@@ -3,6 +3,18 @@ import {myRequest} from 'util/api.js'
 
 Vue.prototype.$myRequest = myRequest //挂载到原型上
 
+//过滤器
+//TODO封装时间转换函数
+Vue.filter('formatDate',(date) => {
+		console.log(date);
+		const nDate = new Date(date)
+		console.log(nDate);
+		const year = nDate.getFullYear()
+		const month = nDate.getMonth().toString().padStart(2,0)//padStart字符串补全，如果小于2位，加一个0
+		const day = nDate.getDate().toString().padStart(2,0)
+		return year+'-'+month+'-'+day
+})
+
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
--- 09 uni-app/hm_shop/pages/news-detail/news-detail.vue
+++ 09 uni-app/hm_shop/pages/news-detail/news-detail.vue
@@ -1,6 +1,14 @@
 <template>
-	<view>
-		资讯详情
+	<view class="news_detail">
+		<text class="title">{{detail.title}}</text>
+		<view class="info">
+			<text>发表时间：{{detail.add_time | formatDate}}</text>
+			<text>浏览：{{detail.click}}</text>
+		</view>
+		<view class="content">
+			<!-- rich-text富文本 -->
+			<rich-text :nodes="detail.content"></rich-text>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +16,40 @@
 	export default {
 		data() {
 			return {
-				
+				id:0,
+				detail:{}
 			}
 		},
 		methods: {
-			
+			async getNewsDetail(){
+				const res = await this.$myRequest({
+					url:'/api/getnew/'+this.id
+				})
+				console.log(res);
+				this.detail = res.data.message[0]
+				console.log(this.detail);
+			}
+		},
+		onLoad(option) {
+			console.log(option);//有id
+			this.id = option.id
+			this.getNewsDetail()
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.news_detail{
+		font-size: 30rpx;
+		padding: 0 20rpx;
+		.title{
+			text-align: center;
+			display: block;//块级元素 为了居中
+			margin: 20rpx;
+		}
+		.info{
+			display: flex;
+			justify-content: space-between;
+		}
+	}
 </style>
