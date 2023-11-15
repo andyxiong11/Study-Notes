67a8062b35e000cc39fb360683a4feb51142c595
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-15 22:50:44 +0800

    jsonp原理p22

---
+++ BBT/10 NodeJS/node-basics/04-buildin-module/03-http/03-jsonp/server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req,res)=>{
+  let urlStr = req.url
+  let urlObj =  url.parse(urlStr,true)
+  console.log(urlStr,urlObj);
+  switch(urlObj.pathname){
+    case '/api/data':
+      // 浏览器上打开http://localhost:8080/api/data就会打印hello，但是如果index.html打开访问，把'hello'当js代码执行控制台就会报错，需要修改为alert('htllo')
+      //res.write('hello')
+
+      //res.write('getData("hello")')
+      res.write(`${urlObj.query.cb}("hello")`)//利用前端传参调用方法
+      break
+    default:
+      res.write('page noe found')
+  }
+  res.end()
+})
+
+server.listen(8080,()=>{
+  console.log('localhost:8080');
+})
