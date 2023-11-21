c465dbcd57578f6f8cb92cf7f34016afc5efe26c
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-21 22:33:59 +0800

    完善静态资源目录搭建

--- 10 NodeJS/node-basics/05-router/satatic/readStaticFile.js
+++ 10 NodeJS/node-basics/05-router/satatic/readStaticFile.js
@@ -15,7 +15,8 @@ function myReadFile(file){
   return new Promise((resolve,reject)=>{
     fs.readFile(file,(err,data)=>{
       if(err){
-        reject('你访问的是一个文件夹，且文件夹没有index.html')
+        // reject('你访问的是一个文件夹，且文件夹没有index.html')
+        resolve('你访问的是一个文件夹，且文件夹没有index.html')//使用resolve为了返回数据给data
       }else{
         resolve(data)
       }
@@ -28,12 +29,12 @@ async function readStaticFile(filePathName){
 
   let ext = path.parse(filePathName).ext//获取文件扩展名
   console.log("filePathName:"+filePathName);
-  let mimeType = mime.getType(ext) //获取文件类型
+  let mimeType = mime.getType(ext) || 'text/html' //获取文件类型，'text/html' 防止访问目录时打印到页面上乱码
   console.log("mimeType:"+mimeType);
 
   let data
   if(fs.existsSync(filePathName)){//fs.existsSync判断文件或文件夹是否存在
-    if(ext){//判断是否文件
+    if(ext){//判断有无扩展名，以此判断是否是文件
       // data = myReadFile(filePathName)
       // 因为报错，需要改造成异步写法
 
@@ -43,7 +44,7 @@ async function readStaticFile(filePathName){
       // 因为取不到data，所以需要使用async await
 
       data  = await myReadFile(filePathName)
-    }else{//目录
+    }else{//是目录，不是文件
       // data = myReadFile(path.join(filePathName,'./index.html'),res)
       // 因为报错，需要改造成异步写法
       
