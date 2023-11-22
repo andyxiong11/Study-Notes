0d8935b0865878777f07dd0350f1fe045b381ad0
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-22 22:03:30 +0800

    获取前端数据 p48 p49

--- BBT/10 NodeJS/express-basics/server.js
+++ BBT/10 NodeJS/express-basics/server.js
@@ -3,6 +3,10 @@ const app = express()
 
 const router = require('./router/index')
 
+// 解析body的中间件，用于解析post请求body中的json和urlencoded类型数据
+app.use(express.json()); 
+app.use(express.urlencoded({ extended:true })); 
+
 app.use('/',router)
 
 app.listen(8080,()=>{
