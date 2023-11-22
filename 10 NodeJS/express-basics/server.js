b17c19f66e80707a741ae85a9d5130e3d5367ec1
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-22 22:45:26 +0800

    利用 Express 托管静态文件 p51

--- 10 NodeJS/express-basics/server.js
+++ 10 NodeJS/express-basics/server.js
@@ -7,6 +7,9 @@ const router = require('./router/index')
 app.use(express.json()); 
 app.use(express.urlencoded({ extended:true })); 
 
+// 静态资源服务中间件（内置中间件）
+app.use(express.static('./public'))
+
 app.use('/',router)
 
 app.listen(8080,()=>{
