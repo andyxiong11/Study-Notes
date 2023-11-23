251aedbcc315ca5aa660275e139129573cebb943
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-23 23:00:08 +0800

    使用art-template模板引擎渲染（p54 06:28)未完成

--- BBT/10 NodeJS/express-basics/server.js
+++ BBT/10 NodeJS/express-basics/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
 const router = require('./router/index')
 
 // 解析body的中间件，用于解析post请求body中的json和urlencoded类型数据
@@ -9,6 +11,14 @@ app.use(express.urlencoded({ extended:true }));
 
 // 静态资源服务中间件（内置中间件）
 app.use(express.static('./public'))
+
+// view engine setup art-template配置
+app.engine('art', require('express-art-template'));
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'production'
+});
+app.set('views', path.join(__dirname, 'view'));//view 模板路径
+app.set('view engine', 'art');
 
 app.use('/',router)
 
