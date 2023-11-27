32f6cbd7d1bb0152cd702947554391bffab20596
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-27 22:21:47 +0800

    服务端使用模板引擎渲染一个页面或生成静态资源页面给前端 p56

--- 10 NodeJS/express-basics/controller/index.js
+++ 10 NodeJS/express-basics/controller/index.js
@@ -1,3 +1,7 @@
+var template = require('art-template');
+var path = require('path')
+var fs = require('fs')
+
 // 应用级中间件
 const list = (req,res,next)=>{
   // SSR 服务端渲染
@@ -19,7 +23,7 @@ const list = (req,res,next)=>{
   res.send(dataObj) */
 
   // 模板引擎渲染
-  let dataArray=[]
+  /* let dataArray=[]
 
   for(var i = 0;i < 100;i++){
     dataArray.push('line'+i)
@@ -29,8 +33,23 @@ const list = (req,res,next)=>{
 
   res.render('list',{
     data:JSON.stringify(dataArray)//此处需要字符串
-  })//模板名字（自动去server.js中配置的view目录查找list.art），模板数据
+  })//模板名字（自动去server.js中配置的view目录查找list.art），模板数据 */
 
+  // 服务端使用模板引擎渲染一个页面返回给前端 使用localhost:8080/api/list访问。主要应用CMS，点击按钮，一键发布新闻静态网页
+  let dataArray=[]
+  for(var i = 0;i < 100;i++){
+    dataArray.push('line'+i)
+  }
+  /* res.render('list-html',{
+    data:dataArray
+  })//模板名字（自动去server.js中配置的view目录查找list-html.art），模板数据 */
+  // 下面是服务端直接生成html返回给前端，而不是res.render返回数据然后解析成html
+  // console.log(template);
+  var html = template(path.join(__dirname, '../view/list-html.art'), {
+    data:dataArray
+  });
+  fs.writeFileSync(path.join(__dirname, '../public/list.html'),html)//生成静态资源页面
+  res.send(html)
 }
 
 exports.list = list
