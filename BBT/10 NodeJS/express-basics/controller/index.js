d28f545b63913f2de26da862744ee0af3326ed85
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-27 22:31:10 +0800

    将数据分离 RMVP模式结束 p57

--- BBT/10 NodeJS/express-basics/controller/index.js
+++ BBT/10 NodeJS/express-basics/controller/index.js
@@ -2,6 +2,8 @@ var template = require('art-template');
 var path = require('path')
 var fs = require('fs')
 
+const listModel = require('../model/list')
+
 // 应用级中间件
 const list = (req,res,next)=>{
   // SSR 服务端渲染
@@ -36,20 +38,23 @@ const list = (req,res,next)=>{
   })//模板名字（自动去server.js中配置的view目录查找list.art），模板数据 */
 
   // 服务端使用模板引擎渲染一个页面返回给前端 使用localhost:8080/api/list访问。主要应用CMS，点击按钮，一键发布新闻静态网页
-  let dataArray=[]
+  /* let dataArray=[]
   for(var i = 0;i < 100;i++){
     dataArray.push('line'+i)
-  }
+  } */ //将数据分离../model/list.js
   /* res.render('list-html',{
     data:dataArray
   })//模板名字（自动去server.js中配置的view目录查找list-html.art），模板数据 */
   // 下面是服务端直接生成html返回给前端，而不是res.render返回数据然后解析成html
   // console.log(template);
   var html = template(path.join(__dirname, '../view/list-html.art'), {
-    data:dataArray
+    // data:dataArray //将数据分离../model/list.js
+    data : listModel.dataArray
   });
   fs.writeFileSync(path.join(__dirname, '../public/list.html'),html)//生成静态资源页面
   res.send(html)
+
+  
 }
 
 exports.list = list
