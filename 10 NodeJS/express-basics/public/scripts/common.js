f493660ff0fbc4f4be83d4caf20458c45832c917
[andyxiong11] <[xiongyexing1999@163.com]>
2023-11-27 21:16:34 +0800

    前端使用art-template模板引擎渲染 p55（前端不展示，渲染失败）

--- 10 NodeJS/express-basics/public/scripts/common.js
+++ 10 NodeJS/express-basics/public/scripts/common.js
@@ -1,7 +1,7 @@
 // console.log('0');
 
 // 以下用于演示 CSR 客户端渲染
-$.ajax({
+/* $.ajax({
   url:'/api/list',
   success(result){
     // console.log(result);
@@ -10,6 +10,27 @@ $.ajax({
       html += '<li>' + value + '</li>'
     })
     html += '</ul>' 
+    $('#list').html(html)//jquery的html方法，将html挂载到页面
+  }
+}) */
+
+// 以下用于演示浏览器客户端使用模板引擎渲染
+$.ajax({
+  url:'/api/list',
+  success(result){
+    let templateStr = `
+      <ul>
+        {{each data}}
+          <li>{{$value}}</li>
+        {{/each}}
+      </ul>
+    `
+
+    let html = template.render(templateStr,{
+      data:result.data
+    })// 模板 数据
+    // console.log(templateStr);
+
     $('#list').html(html)//jquery的html方法，将html挂载到页面
   }
 })
