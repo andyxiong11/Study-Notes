0c1257d529aba2e30b06a05b3a9bbad604b30799
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-04 22:56:51 +0800

    webpack插件安装配置使用 p70

--- 10 NodeJS/lagou-admin/frontend/webpack.config.js
+++ 10 NodeJS/lagou-admin/frontend/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   // 配置环境
@@ -13,5 +15,21 @@ module.exports = {
   output:{
     path: path.join(__dirname,'./dist'),
     filename:'app.js'
+  },
+
+  // 配置插件
+  plugins:[
+    new HtmlWebpackPlugin(),
+    new CopyPlugin({
+      from: './public/*/ico', 
+      to: './dist'
+    })
+  ],
+
+  // 配置server
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,//是否压缩
+    port: 8080,
   }
 }
