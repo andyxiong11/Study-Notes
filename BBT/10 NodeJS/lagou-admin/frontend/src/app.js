05a8bda94656bde4f67830f2581caae898027922
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-07 22:52:35 +0800

    安装“css-loader style-loader”调整样式 p79

--- BBT/10 NodeJS/lagou-admin/frontend/src/app.js
+++ BBT/10 NodeJS/lagou-admin/frontend/src/app.js
@@ -10,5 +10,10 @@ const html = siginTpl({})
 $('#root').html(html) //TODO此处始终报错未定义$，如果将index.html中的js引入放到head中也不显示页面 */
 
 // 以上内容移动到路由文件中
+
+// 载入CSS
+import './assets/common.css'
+
+// 载入路由
 import router from  './routers'
-router.go('/')
+router.go('/index')
