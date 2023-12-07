c5ad1b965dbb4f133833408c245ed871ab681cd9
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-07 22:27:55 +0800

    将路由文件中的操作抽离 p78

--- BBT/10 NodeJS/lagou-admin/frontend/src/routers/index.js
+++ BBT/10 NodeJS/lagou-admin/frontend/src/routers/index.js
@@ -1,20 +1,24 @@
 import SMERouter from 'sme-router'
-import indexTpl from '../views/index.art'
-import siginTpl from '../views/signin.art'
+/* import indexTpl from '../views/index.art'
+import siginTpl from '../views/signin.art' */
 
 const router = new SMERouter('root') //root为index.html的根节点
 
-const htmlIndex = indexTpl({})
-const htmlSignin = siginTpl({})
+/* const htmlIndex = indexTpl({})
+const htmlSignin = siginTpl({}) */
 
-router.route('/', (req, res, next) => {
-  res.render(htmlIndex)
+import {signin,index} from '../controllers'
 
-})
-
-router.route('/sigin', (req, res, next) => {
+/* router.route('/', (req, res, next) => {
   res.render(htmlSignin)
+}) */
+router.route('/', signin(router))//传参router，方便登录页面调用路由方法跳转
 
-})
+router.route('/index',index(router))
+
+/* router.route('/sigin', (req, res, next) => {
+  res.render(htmlSignin)
+}) */
+// router.route('/sigin',signin(router))
 
 export default router
