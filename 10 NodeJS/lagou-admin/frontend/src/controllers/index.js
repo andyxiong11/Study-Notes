2108ec8b1c633b7d282ed489c6dba8a2c9dbbfc1
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-10 22:27:24 +0800

    抽离用户管理为新的模板，引入bootstrap模态框，绑定注册表单提交事件，发起请求 p81

--- 10 NodeJS/lagou-admin/frontend/src/controllers/index.js
+++ 10 NodeJS/lagou-admin/frontend/src/controllers/index.js
@@ -1,14 +1,35 @@
 import indexTpl from '../views/index.art'
-import siginTpl from '../views/signin.art'
+import signinTpl from '../views/signin.art'
+import usersTpl from '../views/users.art'
 
 const htmlIndex = indexTpl({})
-const htmlSignin = siginTpl({})
+const htmlSignin = signinTpl({})
 
 const _handleSubmit = (router) => {
   return (e)=>{
     e.preventDefault();//阻止表单跳转
     router.go('/index')
   }
+}
+
+// 注册表单提交事件
+const _signup = ()=>{
+  const $btnClose = $('#users-close')
+
+  // 提交表单
+  const data = $('#users-form').serialize()
+  console.log(data);
+  $.ajax({
+    url:'/api/users/signup',//后端接口地址
+    type:'post',
+    data,
+    success(res){
+      console.log(res);
+    }
+  })
+
+  // 关闭注册弹窗
+  $btnClose.click()
 }
 
 const signin = (router)=>{
@@ -27,6 +48,14 @@ const index = (router)=>{
 
     // window resize 让页面高度撑满整个屏幕
     $(window,'.wrapper').resize()
+
+    // 填充用户列表
+    /* let users = usersTpl
+    console.log(users); */
+    $('#content').html(usersTpl())
+
+    // 点击保存，提交表单
+    $('#users-save').on('click',_signup)
   }
 }
 
