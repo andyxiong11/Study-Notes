1696aef9c8e2e67b1edfdfede88b0cf78a8900b2
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-11 23:03:48 +0800

    查询数据库用户后，新增分页功能，将前端分页功能抽离 p86

--- 10 NodeJS/lagou-admin/frontend/src/controllers/index.js
+++ 10 NodeJS/lagou-admin/frontend/src/controllers/index.js
@@ -2,6 +2,7 @@ import indexTpl from '../views/index.art'
 import signinTpl from '../views/signin.art'
 import usersTpl from '../views/users.art'
 import usersListTpl from '../views/users-list.art'
+import usersListPagesTpl from '../views/uesers-pages.art'
 
 const htmlIndex = indexTpl({})
 const htmlSignin = signinTpl({})
@@ -34,6 +35,22 @@ const _signup = ()=>{
   $btnClose.click()
 }
 
+// 分页
+const _pagination = (data) => {
+  const pageSize = 10 //每页10条
+  const total = data.length
+  const pageCount = Math.ceil(total/pageSize)//页数
+  const pageArray = new Array(pageCount)// 用页数生成一个数组
+  console.log(pageArray);
+
+  const htmlPage = usersListPagesTpl({
+    // pageCount
+    pageArray
+  })
+
+  $('#users-pages').html(htmlPage)
+}
+
 // 查询用户列表
 const _list = ()=>{
   $.ajax({
@@ -44,6 +61,9 @@ const _list = ()=>{
       $('#users-list').html(usersListTpl({//使用usersListTpl模板将用户数据渲染到页面
         data:result.data
       }))
+
+      // 分页
+      _pagination(result.data)
     }
   })
 }
