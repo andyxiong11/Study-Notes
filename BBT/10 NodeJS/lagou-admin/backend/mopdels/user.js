d6065c9d3e1549da0a28e7c2fe6443a2acf24d00
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-11 22:09:11 +0800

    安装bcrypt，异步将用户密码加密，并在将用户入库时判断用户名在数据库中是否存在；使用cors和webpack配置代理解决前端请求跨域 p84

--- BBT/10 NodeJS/lagou-admin/backend/mopdels/user.js
+++ BBT/10 NodeJS/lagou-admin/backend/mopdels/user.js
@@ -1,12 +1,19 @@
 // 向数据库存数据
 const {Users} = require('../utils/db.js')
 
+const findUser = (username) => {
+  return Users.findOne({username})//在Users集合中查找username，返回一个promise
+}
+
 const signup = ({username,password}) => {
   const users = new Users({
     username,
     password
   })
-  users.save()//保存数据到数据库
+  /* const result = users.save()//保存数据到数据库
+  console.log(result);//result为promise的状态，所以可以直接将result返回 */
+  return users.save()
 }
 
 exports.signup = signup
+exports.findUser = findUser
