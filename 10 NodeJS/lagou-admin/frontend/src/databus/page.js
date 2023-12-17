19bb020b66dc9391ea45806f00563cba1f25a04a
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-17 22:54:41 +0800

    将分页功能的当前页码和每页条数公共变量和更新方法写在类中，做成响应式，修改涉及公共变量读取的写法 p104

--- 10 NodeJS/lagou-admin/frontend/src/databus/page.js
+++ 10 NodeJS/lagou-admin/frontend/src/databus/page.js
@@ -1,4 +1,15 @@
 class Page {
-  static pageSize = 10
-  static curPage = 1
+  /* static pageSize = 10
+  static curPage = 1 */
+  constructor(){
+    this.curPage = 1//当前页码
+    this.pageSize = 5//每页数据数量
+  }
+
+  setCurPage(curPage){
+    // console.log(curPage);
+    this.curPage = curPage
+  }
 }
+
+export default new Page()//将page的实例暴露
