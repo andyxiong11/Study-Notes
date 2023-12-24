54270236e0c09a79ba0e68ab93d2aac3c3f3919e
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-24 22:07:03 +0800

    职位管理前端页面完成 p118

--- BBT/10 NodeJS/lagou-admin/frontend/src/controllers/positions/list-position.js
+++ BBT/10 NodeJS/lagou-admin/frontend/src/controllers/positions/list-position.js
@@ -1,4 +1,10 @@
 // 职位管理 
+import positiosTpl from '../../views/positions.art'
+import positionAddTpl from '../../views/positions-add.art'
+import positionListTpl from '../../views/positions-list.art'
+
+import pagination from '../../components/pagination.js'
+
 import {auth as authModel} from '../../models/auth-index.js'
 
 const listPositions = (router) => {
@@ -6,7 +12,26 @@ const listPositions = (router) => {
     let result = await authModel()//ajax请求
     if(result.ret){//鉴权通过，是登录状态
       next()//因为使用异步的gp21-router替换了sme-router,所以需要next
-      res.render('positions')
+      res.render(positiosTpl())
+
+      // 渲染list
+      $('#positions-list').html(positionListTpl({
+        data:['a','a','a','a','a',]
+      }))
+
+      //分页
+      pagination(['a','b','c','d'])
+
+      // 职位添加
+      $('#positions-list-box').after(positionAddTpl)//after渲染到positions-list-box样式元素之后
+      $('#positions-save').off('click').on('click',()=>{
+        const formbody = $('#position-form').serialize()//serialize获取表单提交的数据
+        console.log(formbody);
+
+        $('#positions-close').click()//关闭
+      })//在positions模板渲染后，绑定添加职位事件
+
+      
     }else{
       router.go('/signin')
     }
