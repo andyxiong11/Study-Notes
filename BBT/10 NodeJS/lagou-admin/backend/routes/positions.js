5751d85935a87781f083f262921d634b32916e23
[andyxiong11] <[xiongyexing1999@163.com]>
2023-12-29 21:44:53 +0800

    修改职位接口基本完成 p132

--- BBT/10 NodeJS/lagou-admin/backend/routes/positions.js
+++ BBT/10 NodeJS/lagou-admin/backend/routes/positions.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const {add,list,remove} = require('../controllers/positions.js')
+const {add,list,remove,update} = require('../controllers/positions.js')
 const uploadMiddleware = require('../middlewares/upload.js')
 
 // router.post('/add',upload.single('companyLogo'),add)//upload.single('companyLogo') multer中间件
@@ -10,5 +10,7 @@ router.post('/add',uploadMiddleware,add)//改造中间件uploadMiddleware
 router.get('/list',list)
 
 router.delete('/remove',remove)
+
+router.patch('/update',uploadMiddleware,update)//uploadMiddleware中间件 上传图片
 
 module.exports = router
