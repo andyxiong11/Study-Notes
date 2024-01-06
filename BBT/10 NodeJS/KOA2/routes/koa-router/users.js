c2dec5a27786d06ff84a09579ce7e8e79334cd53
[andyxiong11] <[xiongyexing1999@163.com]>
2024-01-06 22:58:56 +0800

    koa-router进阶（路由前缀） p146

--- BBT/10 NodeJS/KOA2/routes/koa-router/users.js
+++ BBT/10 NodeJS/KOA2/routes/koa-router/users.js
@@ -1,6 +1,8 @@
 const Router = require('@koa/router')
 
-const router = new Router()
+const router = new Router({
+  prefix:'/abc'//该路由前面需要加前缀才能访问
+})
 
 router.get('/sigin',(ctx,next)=>{
   ctx.body = 'sigin' //返回给前端
