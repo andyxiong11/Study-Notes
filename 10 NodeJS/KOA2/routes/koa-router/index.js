103609d8bc79b03e1d65f13f73fd80e66d8ce9b7
[andyxiong11] <[xiongyexing1999@163.com]>
2024-01-06 22:45:43 +0800

    koa-router进阶（获取下一个中间件返回的信息） p145

--- 10 NodeJS/KOA2/routes/koa-router/index.js
+++ 10 NodeJS/KOA2/routes/koa-router/index.js
@@ -2,8 +2,18 @@ const Router = require('@koa/router');
 
 const router = new Router()
 
-router.get('/',(ctx,next)=>{
-  ctx.body = 'home'
-})
+router
+  .get('/',async (ctx,next)=>{
+    ctx.body = 'home'
+  })
+  //动态路由
+  .get('/:id',async (ctx,next)=>{//:id 随便输
+    ctx.body = ctx.params.id
+    let result = await next()
+    console.log(result);//获取下一个中间件返回的信息
+  },(ctx,next)=>{
+    console.log('0');
+    return 'hello'
+  })
 
 module.exports = router
