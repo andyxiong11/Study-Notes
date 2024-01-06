a5d6ecc866a3e1afcdf7f8f8a6e2aaeb191194bd
[andyxiong11] <[xiongyexing1999@163.com]>
2024-01-06 22:38:54 +0800

    koa-router三方路由中间件使用 p145

--- BBT/10 NodeJS/KOA2/server.js
+++ BBT/10 NodeJS/KOA2/server.js
@@ -13,5 +13,10 @@ koa.use((context,next) => {
 koa.listen(3333);
 //yarn start运行;使用apipost访问端口 */
 
-// 运行路由
-require('./routes/index')
+/* // 运行路由
+require('./routes/index') */
+
+// 三方路由中间件
+const router = require('./routes/koa-router/')
+koa.use(router.routes()).use(router.allowedMethods());
+koa.listen(3333)
