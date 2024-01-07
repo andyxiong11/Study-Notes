ec98390a1ae4758eba41246ed44972894f01396e
andyxiong11 <xiongyexing1999@163.com>
2024-01-07 22:27:25 +0800

    利用koa-bodyparser获取前端请求的内容 p149

--- BBT/10 NodeJS/KOA2/server.js
+++ BBT/10 NodeJS/KOA2/server.js
@@ -13,9 +13,12 @@ koa.use((context,next) => {
 koa.listen(3333);
 //yarn start运行;使用apipost访问端口 */
 
+// koa-bodyparser获取前端请求的内容
+const bodyParser = require('koa-bodyparser'); 
+koa.use(bodyParser());//需要在路由使用之前使用
+
 /* // 运行路由
 require('./routes/index') */
-
 // 三方路由中间件
 const router = require('./routes/koa-router/')
 koa.use(router.routes()).use(router.allowedMethods());
