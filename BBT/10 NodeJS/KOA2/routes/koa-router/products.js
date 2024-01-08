b64b7b35fc8e94c12f0f03adb07ec557450c8193
[andyxiong11] <[xiongyexing1999@163.com]>
2024-01-08 21:03:08 +0800

    koa-views模板引擎渲染 p151

--- BBT/10 NodeJS/KOA2/routes/koa-router/products.js
+++ BBT/10 NodeJS/KOA2/routes/koa-router/products.js
@@ -6,10 +6,17 @@ router.get('/list',(ctx,next)=>{
   ctx.body = ['t-shit','shoes']
 })
 
-router.post('/add',(ctx,next)=>{
+router.post('/add',async (ctx,next)=>{
   const data = ctx.request.body
   // console.log(data);
-  ctx.body = data//将前端请求的数据返回给前端
+  // ctx.body = data//将前端请求的数据返回给前端
+  
+  /* await ctx.render('succ',{//succ 模板文件名
+    data: JSON.stringify(data)//返回一个字符串对象
+  }) */
+  await ctx.render('fail.html',{//fail.html 模板文件名
+    message: 'fail'
+  })
 })
 
 module.exports = router
